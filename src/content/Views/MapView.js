02cca2069ad623e239a296bf4587524127e2a7e1
Andrei <andrei.tudor27@gmail.com>
2018-03-22 14:20:48 +0100

    Integrate MapView and improve code

--- src/content/Views/MapView.js
+++ src/content/Views/MapView.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-
+import MapContainer from '../Map/MapContainer'
 export default class MapView extends Component {
 	render() {
 		return (
-			<div>
 
-			</div>
+			<MapContainer />
 		)
 	}
 }
