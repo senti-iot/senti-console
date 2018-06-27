8ceb3cca0d8052b6a1bfa3d715ab801903574f17
Andrei <andrei.tudor27@gmail.com>
2018-06-27 20:32:21 +0200

    Change counter to 200

--- src/components/Devices/CounterModal.js
+++ src/components/Devices/CounterModal.js
@@ -43,7 +43,7 @@ class CounterModal extends React.Component {
 		super(props)
 
 		this.state = {
-			count: 5, //change
+			count: 200, //change
 			open: false,
 			timer: 0,
 			timestamp: null,
@@ -81,7 +81,7 @@ class CounterModal extends React.Component {
 		this.timeCounter = null
 		this.setState({
 			timer: 0,
-			count: 5, //change
+			count: 200, //change
 			timestamp: null
 		})
 	}
@@ -166,7 +166,7 @@ class CounterModal extends React.Component {
 
 								<ItemGrid>
 									<Button
-										disabled={ this.state.count === 0 ? false : this.state.count < 5 ? true : false}//change
+										disabled={ this.state.count === 0 ? false : this.state.count < 200 ? true : false}//change
 										color={"primary"}
 										variant="contained"
 										onClick={this.state.count === 0 ? this.handleFinish : this.handleStart}>
