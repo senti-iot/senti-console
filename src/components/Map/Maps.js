bf4f9e9da968be2992f2febc65cb750d22cd97fd
Andrei Tudor <andrei.tudor27@gmail.com>
2018-07-12 10:59:22 +0200

    Marker Icons with Status Color

--- src/components/Map/Maps.js
+++ src/components/Map/Maps.js
@@ -6,16 +6,16 @@ import {
 	GoogleMap,
 	Marker
 } from "react-google-maps";
-// import { PinDrop } from '@material-ui/icons'
 import CircularLoader from "../Loader/CircularLoader";
 import MarkerClusterer from "react-google-maps/lib/components/addons/MarkerClusterer";
+import { MarkerIcon } from './MarkerIcon'
 
 export const Maps = compose(
 	withProps({
 		googleMapURL:
 			"https://maps.googleapis.com/maps/api/js?key=" + process.env.REACT_APP_SENTI_MAPSKEY + "&v=3.exp&libraries=geometry,drawing,places",
-		loadingElement: <CircularLoader notCentered/>,
-		containerElement: <div style={{ height: `calc(100vh - 268px)`, width: '100%'  }} />,
+		loadingElement: <CircularLoader notCentered />,
+		containerElement: <div style={{ height: `calc(100vh - 268px)`, width: '100%' }} />,
 		mapElement: <div id={'map'} style={{ height: `100%` }}>HelloWorld</div>
 	}),
 	withScriptjs,
@@ -23,11 +23,13 @@ export const Maps = compose(
 )(props => {
 	let defaultLat = parseFloat(55.298) //Denmark,
 	let defaultLng = parseFloat(10.605) //Denmark
-	if (!props.centerDenmark)
-	{	defaultLat = props.markers[0] ? props.markers[0].lat : defaultLat
+	if (!props.centerDenmark) {
+		defaultLat = props.markers[0] ? props.markers[0].lat : defaultLat
 		defaultLng = props.markers[0] ? props.markers[0].long : defaultLng
 	}
-	return <GoogleMap defaultZoom={props.zoom ? props.zoom : 7} defaultCenter={{ lat: defaultLat, lng: defaultLng } }>
+
+
+	return <GoogleMap defaultZoom={props.zoom ? props.zoom : 7} defaultCenter={{ lat: defaultLat, lng: defaultLng }}>
 
 		<MarkerClusterer
 			onClick={props.onMarkerClustererClick}
@@ -36,7 +38,8 @@ export const Maps = compose(
 			gridSize={10}
 		>
 			{props.markers.length > 0 ? props.markers.map((m, i) => {
-				return <Marker /* label={m.device_id.toString()} */	/*  icon={{ url: svg }}*/ onClick = {() => alert('bing')} key={i} position={{ lat: m.lat, lng: m.long }} />
+				console.log(m)
+				return <Marker icon={{ url: `data:image/svg+xml,${MarkerIcon(m.liveStatus)}` }} onClick={() => alert('bing')} key={i} position={{ lat: m.lat, lng: m.long }} />
 			})
 				: null}
 		</MarkerClusterer>
