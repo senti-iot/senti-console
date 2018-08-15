2122107ff01c74439a1e8cc8e767d3e933438fb4
Mikkel Broberg <mb@webhouse.dk>
2018-08-15 13:30:57 +0200

    PerfectScrollbar fix

--- src/layouts/Dashboard/Dashboard.js
+++ src/layouts/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Switch, Route, Redirect } from "react-router-dom";
 // creates a beautiful scrollbar
-// import PerfectScrollbar from "perfect-scrollbar";
+import PerfectScrollbar from "perfect-scrollbar";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 import { withStyles } from "@material-ui/core";
 import { Header, /* Footer, */ Sidebar, CircularLoader } from "components";
