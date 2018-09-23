b762f57a75dd032f948da88adda49c0f0eae1b48
Andrei <andrei.tudor27@gmail.com>
2018-09-23 23:27:31 +0200

    Double Escape Closes The Search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -88,12 +88,24 @@ class IntegrationAutosuggest extends React.Component {
 		this.inputRef = React.createRef()
 
 	}
-
+	// componentDidMount() {
+	// 	document.addEventListener("keydown", this.handleKeyPress, false);
+	// }
+	// componentWillUnmount() {
+	// 	document.removeEventListener("keydown", this.handleKeyPress, false);
+	// }
 	handleResetSearch = () => {
 		this.handleChange(null, { newValue: '' })
 
 	}
-	handleSuggestionsFetchRequested = ({ value }) => {
+	handleSuggestionsFetchRequested = ({ value, reason }) => {
+		const { open } = this.state
+		const { searchValue } = this.props
+		if (open && searchValue === "" && reason === "escape-pressed") {
+			// console.log("reset", open, searchValue, open && searchValue === "" ? true : false)
+			this.handleClose()
+			// }
+		}
 		this.setState({
 			suggestions: getSuggestions(value, this.props.suggestions),
 		});
@@ -105,7 +117,9 @@ class IntegrationAutosuggest extends React.Component {
 		});
 	};
 
-	handleChange = (event, { newValue }) => {
+	handleChange = (event, { newValue, method }) => {
+		// console.log(method)
+	
 		this.props.handleFilterKeyword(newValue)
 	};
 	focusInput = () => {
@@ -119,6 +133,19 @@ class IntegrationAutosuggest extends React.Component {
 	handleClose = () => {
 		this.setState({ open: false })
 	}
+	// handleKeyPress = (event) => {
+	// 	event.stopPropagation()
+	
+	// 	// console.log(event.key, searchValue)
+	// 	if (event.key === 'Escape') {
+	// 	// if (searchValue !== "") {
+	// 	// 	console.log("reset", searchValue)
+	// 	// 	this.handleResetSearch()
+	// 	// }
+	// 		// else {
+		
+	// 	}
+	// }
 	render() {
 		const { classes, right } = this.props;
 
@@ -127,13 +154,15 @@ class IntegrationAutosuggest extends React.Component {
 
 				<ClickAwayListener onClickAway={this.handleClose}>
 					<Autosuggest
+
 						theme={{
 							container: classes.container + " " + (right ? classes.right : ''),
 							suggestionsContainerOpen: classes.suggestionsContainerOpen,
 							suggestionsList: classes.suggestionsList,
 							suggestion: classes.suggestion,
 						}}
-						focusInputOnSuggestionClick={false}
+						alwaysRenderSuggestions={true}
+						focusInputOnSuggestionClick={true}
 						renderInputComponent={renderInput}
 						suggestions={this.state.suggestions}
 						onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
@@ -151,6 +180,7 @@ class IntegrationAutosuggest extends React.Component {
 							handleOpen: this.handleOpen,
 							handleClose: this.handleClose,
 							handleResetSearch: this.handleResetSearch,
+							// onKeyPress: this.handleKeyPress,
 							t: this.props.t
 						}}
 					/>
