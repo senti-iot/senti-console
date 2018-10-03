d1b3ffa8ac0f56b8a92f6948aad3b872a400bbc1
Andrei Tudor <andrei.tudor27@gmail.com>
2018-10-03 16:02:31 +0200

    EditUser

--- src/views/Users/User.js
+++ src/views/Users/User.js
@@ -45,17 +45,17 @@ class User extends Component {
 			}
 	}
 
-	componentDidMount = () => {
+	componentDidMount =async  () => {
 		if (this.props.match) {
 			if (this.props.match.params.id) {
-				this.timer = setTimeout(async () => await getUser(this.props.match.params.id).then(async rs => {
+				await getUser(this.props.match.params.id).then(async rs => {
 					if (rs.id === null)
 						this.props.history.push('/404')
 					else {
 						this.props.setHeader(`${rs.firstName} ${rs.lastName}`, true, '/users', "users")
 						this.setState({ user: rs, loading: false })
 					}
-				}))
+				})
 			}
 		}
 		else {
