f9696315f2821a8de5d0a4fed8b20ea26755c7fc
Andrei Tudor <andrei.tudor27@gmail.com>
2018-10-09 12:15:35 +0200

    Checkpoint

--- src/components/Collections/CollectionTable.js
+++ src/components/Collections/CollectionTable.js
@@ -1,16 +1,16 @@
 import {
-	Checkbox, Hidden, Paper, Table, TableBody, TableCell,
+	Checkbox, Hidden, Table, TableBody, TableCell,
 	TableRow, withStyles, DialogTitle, Dialog, DialogContent,
-	DialogContentText, DialogActions, Button, /* MenuItem, Menu, */ IconButton, ListItem, ListItemIcon, ListItemText, List,
+	DialogContentText, DialogActions, Button, /* MenuItem, Menu, IconButton,*/  ListItem, ListItemIcon, ListItemText, List,
 } from "@material-ui/core"
 import TC from 'components/Table/TC'
-import { Delete, Edit, PictureAsPdf, Add } from 'variables/icons'
+import { Delete, Edit, PictureAsPdf/*  Add */ } from 'variables/icons'
 import devicetableStyles from "assets/jss/components/devices/devicetableStyles"
 import PropTypes from "prop-types"
 import React, { Fragment } from "react"
 import { withRouter } from 'react-router-dom'
 import EnhancedTableHead from 'components/Table/TableHeader'
-import EnhancedTableToolbar from 'components/Table/TableToolbar'
+// import EnhancedTableToolbar from 'components/Table/TableToolbar'
 import { connect } from "react-redux"
 import TP from 'components/Table/TP'
 import { Info, Caption, ItemG } from "components";
@@ -21,7 +21,7 @@ class CollectionTable extends React.Component {
 		super(props);
 
 		this.state = {
-			selected: [],
+			// selected: [],
 			page: 0,
 			rowsPerPage: props.rowsPerPage,
 			anchorElMenu: null,
@@ -44,43 +44,45 @@ class CollectionTable extends React.Component {
 		this.props.handleRequestSort(event, property)
 	}
 
-	handleSelectAllPage = (event, checked) => {
-		if (checked) {
-			const { data } = this.props
-			const { rowsPerPage, page } = this.state
-			this.setState({ selected: data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => n.id) })
-			return;
-		}
-	}
+	// handleSelectAllPage = (event, checked) => {
+	// 	if (checked) {
+	// 		const { data } = this.props
+	// 		const { rowsPerPage, page } = this.state
+	// 		this.setState({ selected: data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => n.id) })
+	// 		return;
+	// 	}
+	// }
 
 	handleSelectAllClick = (event, checked) => {
-		if (checked) {
-			this.setState({ selected: this.props.data.map(n => n.id) })
-			return;
-		}
-		this.setState({ selected: [] })
+		this.props.handleSelectAllClick(event, checked)
+		// if (checked) {
+		// this.setState({ selected: this.props.data.map(n => n.id) })
+		// return;
+		// }
+		// this.setState({ selected: [] })
 	}
 
 	handleClick = (event, id) => {
-		event.stopPropagation()
-		const { selected } = this.state;
-		const selectedIndex = selected.indexOf(id)
-		let newSelected = [];
+		this.props.handleClick(event, id)
+		// event.stopPropagation()
+		// const { selected } = this.state;
+		// const selectedIndex = selected.indexOf(id)
+		// let newSelected = [];
 
-		if (selectedIndex === -1) {
-			newSelected = newSelected.concat(selected, id);
-		} else if (selectedIndex === 0) {
-			newSelected = newSelected.concat(selected.slice(1))
-		} else if (selectedIndex === selected.length - 1) {
-			newSelected = newSelected.concat(selected.slice(0, -1))
-		} else if (selectedIndex > 0) {
-			newSelected = newSelected.concat(
-				selected.slice(0, selectedIndex),
-				selected.slice(selectedIndex + 1),
-			);
-		}
+		// if (selectedIndex === -1) {
+		// 	newSelected = newSelected.concat(selected, id);
+		// } else if (selectedIndex === 0) {
+		// 	newSelected = newSelected.concat(selected.slice(1))
+		// } else if (selectedIndex === selected.length - 1) {
+		// 	newSelected = newSelected.concat(selected.slice(0, -1))
+		// } else if (selectedIndex > 0) {
+		// 	newSelected = newSelected.concat(
+		// 		selected.slice(0, selectedIndex),
+		// 		selected.slice(selectedIndex + 1),
+		// 	);
+		// }
 
-		this.setState({ selected: newSelected })
+		// this.setState({ selected: newSelected })
 	}
 
 	handleChangePage = (event, page) => {
@@ -100,14 +102,13 @@ class CollectionTable extends React.Component {
 		this.setState({ openDelete: false })
 	}
 	handleDeleteCollection = async () => {
-		await this.props.handleDeleteCollections(this.state.selected)
+		await this.props.handleDeleteCollections(this.props.selected)
 		this.setState({
-			selected: [],
 			anchorElMenu: null,
 			openDelete: false
 		})
 	}
-	isSelected = id => this.state.selected.indexOf(id) !== -1
+	isSelected = id => this.props.selected.indexOf(id) !== -1
 
 	handleEdit = () => {
 		this.props.history.push(`/collection/${this.state.selected[0]}/edit`)
@@ -127,20 +128,20 @@ class CollectionTable extends React.Component {
 	}
 	addNewCollection = () => { this.props.history.push('/collections/new') }
 
-	renderTableToolBarContent = () => {
-		const { accessLevel } = this.props
-		// const { anchorFilterMenu } = this.state
-		let access = accessLevel.apicollection ? accessLevel.apicollection.edit ? true : false : false
-		return <Fragment>
-			{access ? <IconButton aria-label="Add new collection" onClick={this.addNewCollection}>
-				<Add />
-			</IconButton> : null
-			}
-		</Fragment>
-	}
+	// renderTableToolBarContent = () => {
+	// 	const { accessLevel } = this.props
+	// 	// const { anchorFilterMenu } = this.state
+	// 	let access = accessLevel.apicollection ? accessLevel.apicollection.edit ? true : false : false
+	// 	return <Fragment>
+	// 		{access ? <IconButton aria-label="Add new collection" onClick={this.addNewCollection}>
+	// 			<Add />
+	// 		</IconButton> : null
+	// 		}
+	// 	</Fragment>
+	// }
 	renderConfirmDelete = () => {
-		const { openDelete, selected } = this.state
-		const { data, t, classes } = this.props
+		const { openDelete  } = this.state
+		const { data, t, classes, selected } = this.props
 		return <Dialog
 			open={openDelete}
 			onClose={this.handleCloseDeleteDialog}
@@ -170,15 +171,15 @@ class CollectionTable extends React.Component {
 
 
 	render() {
-		const { classes, t, order, orderBy, data } = this.props
-		const { selected, rowsPerPage, page } = this.state
+		const { classes, t, order, orderBy, data, selected } = this.props
+		const { rowsPerPage, page } = this.state
 		let emptyRows;
 		if (data)
 			emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage)
-		console.log(data)
 		return (
+			<Fragment>
 
-			<Paper className={classes.root}>
+				{/*<Paper className={classes.root}>
 				<EnhancedTableToolbar //	./TableToolbar.js
 					anchorElMenu={this.state.anchorElMenu}
 					handleToolbarMenuClose={this.handleToolbarMenuClose}
@@ -187,7 +188,7 @@ class CollectionTable extends React.Component {
 					options={this.options}
 					t={t}
 					content={this.renderTableToolBarContent()}
-				/>
+				/> */}
 				<div className={classes.tableWrapper}>
 					<Table className={classes.table} aria-labelledby="tableTitle">
 						<EnhancedTableHead // ./ProjectTableHeader
@@ -231,8 +232,8 @@ class CollectionTable extends React.Component {
 														</Info>
 														<ItemG>
 															<Caption noWrap className={classes.noMargin}>
-																
-																{`${n.org ? n.org.name : ""}`} 
+
+																{`${n.org ? n.org.name : ""}`}
 															</Caption>
 														</ItemG>
 													</ItemG>
@@ -249,7 +250,7 @@ class CollectionTable extends React.Component {
 											<TC label={n.description} />
 											<TC label={dateFormatter(n.created)} />
 											<TC label={dateFormatter(n.modified)} />
-											<TC label={n.org ? n.org.name : ""} /> 
+											<TC label={n.org ? n.org.name : ""} />
 
 										</Hidden>
 									</TableRow>
@@ -273,7 +274,8 @@ class CollectionTable extends React.Component {
 					handleChangeRowsPerPage={this.handleChangeRowsPerPage}
 				/>
 				{this.renderConfirmDelete()}
-			</Paper>
+			</Fragment>
+
 		)
 	}
 }
