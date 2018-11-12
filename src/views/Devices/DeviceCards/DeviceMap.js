df5aeb8dfb2d2da9afc5726de4fe690b87e8dc82
Andrei <andrei.tudor27@gmail.com>
2018-11-12 09:59:25 +0100

    Device Map Menu

--- src/views/Devices/DeviceCards/DeviceMap.js
+++ src/views/Devices/DeviceCards/DeviceMap.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
-import { InfoCard } from 'components';
+import { InfoCard, Dropdown } from 'components';
 import { Map } from 'variables/icons'
 import { Grid } from '@material-ui/core';
 import { Maps } from 'components/Map/Maps';
 
 export default class DeviceMap extends Component {
+	constructor(props) {
+	  super(props)
+	
+	  this.state = {
+		 heatMap: false
+	  }
+	}
+	
+	renderMenu = () => {
+		const { classes, t } = this.props
+		return <Dropdown menuItems={
+			[
+				{ label: t("actions.heatMap"), icon: <Map className={classes.leftIcon} />, func: () => this.setState({ heatMap: !this.state.heatMap }) },
+			]
+		} />
+	}
 	render() {
 		const { device, weather, t } = this.props
 		return (
@@ -12,6 +28,7 @@ export default class DeviceMap extends Component {
 				title={t("devices.cards.map")}
 				subheader={t("devices.fields.coordsW", { lat: device.lat, long: device.long })}
 				avatar={<Map />}
+				topAction={this.renderMenu()}
 				noExpand
 				content={
 					<Grid container justify={'center'}>
