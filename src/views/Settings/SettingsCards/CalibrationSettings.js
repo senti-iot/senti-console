e2103dfe38708c00307a232af1fb6750d6ef8a62
Andrei Tudor <andrei.tudor27@gmail.com>
2018-12-07 12:09:37 +0100

    Redux Count by time

--- src/views/Settings/SettingsCards/CalibrationSettings.js
+++ src/views/Settings/SettingsCards/CalibrationSettings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
-import { InfoCard, ItemGrid, DSelect, Caption } from 'components';
+import { InfoCard, ItemGrid, DSelect, /* Caption */ } from 'components';
 import { Build } from 'variables/icons'
 import { Grid, ListItem, List, ListItemText, withStyles, Collapse } from '@material-ui/core';
 import { settingsStyles } from 'assets/jss/components/settings/settingsStyles';
@@ -12,25 +12,25 @@ import DInput from 'components/CustomInput/DInput';
 class CalibrationSettings extends Component {
 
 	changeCalType = e => {
-		this.props.changeCount(0)//Temporary
 		this.props.changeCalType(e.target.value)
 	}
-	changeMinutesCount = e => this.props.changeCount(e.target.value)
+	changeMinutesCount = val => this.props.changeTCount(val)
 	changeCalNotif = e => this.props.changeCalNotif(e.target.value)
 	changeCount = val => this.props.changeCount(val)
 
 	render() {
 		const { classes, t } = this.props
-		const { calibration, count, calNotifications } = this.props
+		const { calibration, count, tcount, calNotifications } = this.props
 		const calibrations = [
 			{ value: 0, label: t('settings.calibration.time') },
 			{ value: 1, label: t('settings.calibration.count') }
 		]
 		const counts = [ 200, 10, 20, 30, 40, 50]
 		const minutes = [
-			{ value: 0, label: '10 ' + t('settings.calibration.minutes') },
-			{ value: 1, label: '20 ' + t('settings.calibration.minutes') },
-			{ value: 3, label: '30 ' + t('settings.calibration.minutes') }
+			{ value: 300, label: `5 ${t('settings.calibration.minutes')}` },
+			{ value: 600, label: `10 ${t('settings.calibration.minutes')}` },
+			{ value: 1200, label: `20 ${t('settings.calibration.minutes')}` },
+			{ value: 1800, label: '30 ' + t('settings.calibration.minutes') }
 		]
 		const notifications = [
 			{ value: 0, label: t('settings.calibration.3months') },
@@ -64,7 +64,7 @@ class CalibrationSettings extends Component {
 								<ListItem divider>
 									<ItemGrid container zeroMargin noPadding alignItems={'center'}>
 										<ListItemText>{t('settings.calibration.byTime')}</ListItemText>
-										<DSelect menuItems={minutes} value={count} onChange={this.changeMinutesCount} />
+										<DInput menuItems={minutes} value={tcount} onChange={this.changeMinutesCount} />
 									</ItemGrid>
 								</ListItem>
 							</Collapse>
@@ -74,12 +74,12 @@ class CalibrationSettings extends Component {
 									<DSelect menuItems={notifications} value={calNotifications} onChange={this.changeCalNotif} />
 								</ItemGrid>
 							</ListItem>
-							<ListItem >
+							{/* <ListItem >
 								<ItemGrid container zeroMargin noPadding alignItems={'center'}>
 									<ListItemText>{t('settings.calibration.history')}</ListItemText>
 									<Caption>*Work in progress*</Caption>
 								</ItemGrid>
-							</ListItem>
+							</ListItem> */}
 						</List>
 					</Grid>
 				}
