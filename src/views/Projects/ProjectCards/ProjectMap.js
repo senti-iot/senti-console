e0e6c0b16967d49f28ab3ce3ac0ec1eb0ec9618d
Andrei Tudor <andrei.tudor27@gmail.com>
2018-12-18 11:20:46 +0100

    Change Map Theme on Collections and Project

--- src/views/Projects/ProjectCards/ProjectMap.js
+++ src/views/Projects/ProjectCards/ProjectMap.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { InfoCard, Caption, Dropdown } from 'components';
-import { Map } from 'variables/icons'
-import { Grid, Checkbox } from '@material-ui/core';
+import React, { Component, Fragment } from 'react'
+import { InfoCard, Caption, Dropdown, ItemG } from 'components';
+import { Map, Layers } from 'variables/icons'
+import { Grid, Checkbox, MenuItem, Menu, IconButton } from '@material-ui/core';
 import OpenStreetMap from 'components/Map/OpenStreetMap';
 
 export default class ProjectMap extends Component {
@@ -9,20 +9,65 @@ export default class ProjectMap extends Component {
 		super(props)
 
 		this.state = {
-			heatMap: false
+			heatMap: false,
+			actionAnchorVisibility: null,
+			mapTheme: props.mapTheme
 		}
 	}
-
+	visibilityOptions = [
+		{ id: 0, icon: "", label: "Humanitarian" },
+		{ id: 1, icon: "", label: "Luftphoto 2018" },
+		{ id: 2, icon: "", label: "Dark" },
+		{ id: 3, icon: "", label: "Stamen Toner" },
+		{ id: 4, icon: "", label: "Stamen Watercolor" },
+		{ id: 5, icon: "", label: "Carto" },
+		{ id: 6, icon: "", label: "OpenStreetMap" }
+	]
+	handleVisibility = e => (event) => {
+		if (event)
+			event.preventDefault()
+		this.setState({ mapTheme: e, actionAnchorVisibility: null })
+	}
+	handleOpenMenu = e => {
+		this.setState({ actionAnchorVisibility: e.currentTarget })
+	}
+	handleCloseMenu = e => {
+		this.setState({ actionAnchorVisibility: null })
+	}
 	renderMenu = () => {
 		const { t } = this.props
-		return <Dropdown menuItems={
-			[
-				{ label: t('actions.heatMap'), icon: <Checkbox checked={this.state.heatMap} />, func: () => this.setState({ heatMap: !this.state.heatMap }) },
-			]
-		} />
+		const { actionAnchorVisibility, mapTheme } = this.state
+		return <Fragment>
+			<ItemG>
+				<IconButton title={'Map layer'} variant={'fab'} onClick={this.handleOpenMenu}>
+					<Layers />
+				</IconButton>
+				<Menu
+					id='long-menu2'
+					anchorEl={actionAnchorVisibility}
+					open={Boolean(actionAnchorVisibility)}
+					onClose={this.handleCloseMenu}
+					PaperProps={{
+						style: {
+							minWidth: 250
+						}
+					}}>
+					{this.visibilityOptions.map(op => {
+						return <MenuItem key={op.id} value={op.id} button onClick={this.handleVisibility(op.id)} selected={mapTheme === op.id ? true : false}>
+							{op.label}
+						</MenuItem>
+					})}
+					{/* </List> */}
+				</Menu>
+			</ItemG><Dropdown menuItems={
+				[{ label: t('actions.heatMap'), icon: <Checkbox checked={this.state.heatMap} />, func: () => this.setState({ heatMap: !this.state.heatMap }) }, ]
+			} />
+		</Fragment>
 	}
 	render() {
-		const { devices, t, mapTheme } = this.props
+		const { devices, t } = this.props
+		const { mapTheme } = this.state
+
 		return (
 			<InfoCard
 				title={t('devices.cards.map')}
