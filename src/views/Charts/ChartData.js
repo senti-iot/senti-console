13bc593cc8e27f036ffe7f9f584b8f2ea462f3ff
Andrei Tudor <andrei.tudor27@gmail.com>
2019-02-27 16:21:29 +0100

    Smarter fix for Pies

--- src/views/Charts/ChartData.js
+++ src/views/Charts/ChartData.js
@@ -186,7 +186,7 @@ class ChartData extends PureComponent {
 	}
 
 	renderType = () => {
-		const { title, setHoverID, t, device, period, single, hoverID, periods } = this.props
+		const { title, setHoverID, t, device, period, single, hoverID } = this.props
 		const { loading } = this.state
 		if (!loading) {
 			const { roundDataSets, lineDataSets, barDataSets } = this.state
@@ -195,10 +195,10 @@ class ChartData extends PureComponent {
 					return roundDataSets ?
 						<ItemG container >
 							{roundDataSets.map((d, i) => {
-								return <ItemG style={{ marginBottom: 30 }} key={i} xs={12} md={roundDataSets.length >= 2 ? periods.length > 2 ? 12 :  6 : 12} direction={'column'} container justify={'center'}>
-									<div style={{ maxHeight: 300 }}>
+								return <ItemG style={{ marginBottom: 30 }} key={i} xs={12} md/* md={roundDataSets.length >= 2 ? periods.length > 2 ? 12 : 6 : 12} */ direction={'column'} container justify={'center'}>
+									<div style={{ maxHeight: 200 }}>
 										<PieChart
-											height={300}
+											height={200}
 											title={title}
 											single
 											unit={this.timeTypes[period.timeType]}
@@ -216,10 +216,10 @@ class ChartData extends PureComponent {
 					return roundDataSets ?
 						<ItemG container >
 							{roundDataSets.map((d, i) => {
-								return <ItemG style={{ marginBottom: 30 }} key={i} xs={12} md={roundDataSets.length >= 2 ? periods.length > 2 ? 12 : 6 : 12} direction={'column'} container justify={'center'}>
-									<div style={{ maxHeight: 300 }}>
+								return <ItemG style={{ marginBottom: 30 }} key={i} xs={12} md direction={'column'} container justify={'center'}>
+									<div style={{ maxHeight: 200 }}>
 										<DoughnutChart
-											height={300}
+											height={200}
 											title={title}
 											single
 											unit={this.timeTypes[period.timeType]}
@@ -440,7 +440,6 @@ class ChartData extends PureComponent {
 	}
 }
 const mapStateToProps = (state) => ({
-	periods: state.dateTime.periods
 })
 
 const mapDispatchToProps = dispatch => ({
