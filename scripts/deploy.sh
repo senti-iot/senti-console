cb3a84b12e073c1908a7ffdee2d79d9613790ff8
Andrei Tudor <andrei.tudor27@gmail.com>
2020-01-20 16:26:00 +0100

    Try again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ "$1" == "master" ]]; then 
+if [[ "$1" == "master" ]]; then
 	echo
-	echo Deploying Senti App $1 ... 
+	echo Deploying Senti App $1 ...
 	# rsync -r --quiet $2/build/ ubuntu@kanata.webhouse.net:/srv/www/odeumcode/dashboard.senti.cloud
 	rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/odeumcode/dashboard.senti.cloud
 	echo
@@ -10,16 +10,28 @@ if [[ "$1" == "master" ]]; then
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti App MASTER updated!"}' https://hooks.slack.com/services/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
 	echo Deployment to production done!
 	exit 0
-fi 
+fi
 
-if [[ "$1" == "beta" ]]; then 
+if [[ "$1" == "beta" ]]; then
 	echo
-	echo Deploying Senti App $1 ... 
+	echo Deploying Senti App $1 ...
 	# rsync -r --quiet $2/build/ ubuntu@kanata.webhouse.net:/srv/www/odeumcode/beta.senti.cloud
 	rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/odeumcode/beta.senti.cloud
 	echo
 	# Senti Workspace
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti App BETA updated!"}' https://hooks.slack.com/services/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
+	echo Deployment to beta done!
+	exit 0
+fi
+
+if [[ "$1" == "alpha" ]]; then
+	echo
+	echo Deploying Senti App $1 ...
+	# rsync -r --quiet $2/build/ ubuntu@kanata.webhouse.net:/srv/www/odeumcode/beta.senti.cloud
+	# rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/odeumcode/beta.senti.cloud
+	echo
+	# Senti Workspace
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti App BETA updated!"}' $SLACK_HOOK
 	echo Deployment to beta done!
 	exit 0
 fi
