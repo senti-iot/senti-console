38605bf7b57cb0b3d645b037c4429331b11c3e3a
SromPavel <1074227@ucn.dk>
2020-01-29 13:14:59 +0100

    CollectionTable.js => hooks

--- src/components/Collections/CollectionTable.js
+++ src/components/Collections/CollectionTable.js
@@ -5,10 +5,10 @@ import {
 import TC from 'components/Table/TC'
 import devicetableStyles from 'assets/jss/components/devices/devicetableStyles'
 import PropTypes from 'prop-types'
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import TableHeader from 'components/Table/TableHeader'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import TP from 'components/Table/TP'
 import { Info, Caption, ItemG } from 'components';
 import { dateFormatter } from 'variables/functions';
@@ -16,71 +16,84 @@ import { SignalWifi2Bar } from 'variables/icons'
 import CollectionHover from 'components/Hover/CollectionHover';
 import { getCollection } from 'variables/dataCollections';
 import { getProject } from 'variables/dataProjects';
-class CollectionTable extends React.Component {
-	constructor(props) {
-		super(props);
 
-		this.state = {
-			page: 0,
-		}
+// const mapStateToProps = (state) => ({
+// 	rowsPerPage: state.appState.trp ? state.appState.trp : state.settings.trp,
+// 	language: state.localization.language,
+// 	accessLevel: state.settings.user.privileges,
+// 	hoverTime: state.settings.hoverTime
+// })
+
+const CollectionTable = props => {
+	const rowsPerPage = useSelector(state => state.appState.trp ? state.appState.trp : state.settings.trp)
+	// const language = useSelector(state => state.localization.language)
+	// const accessLevel = useSelector(state => state.settings.user.privileges)
+	const hoverTime = useSelector(state => state.settings.hoverTime)
+
+	const [page, setPage] = useState(0)
+	const [rowHover, setRowHover] = useState(null) // added
+	const [hoverCollection, setHoverCollection] = useState(null) // added
+
+	let timer = null
+
+	const handleRequestSort = (event, property) => {
+		props.handleRequestSort(event, property)
 	}
 
-	timer = null
-
-	handleRequestSort = (event, property) => {
-		this.props.handleRequestSort(event, property)
+	const handleSelectAllClick = (event, checked) => {
+		props.handleSelectAllClick(event, checked)
 	}
 
-	handleSelectAllClick = (event, checked) => {
-		this.props.handleSelectAllClick(event, checked)
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage)
 	}
 
-	handleChangePage = (event, page) => {
-		this.setState({ page });
-	}
+	const isSelectedFunc = id => props.selected.indexOf(id) !== -1
 
-	isSelected = id => this.props.selected.indexOf(id) !== -1
-
-	setHover = async (e, n) => {
+	const setHover = async (e, n) => {
 		e.persist()
-		const { hoverTime } = this.props
-		const { rowHover } = this.state
 		if (hoverTime > 0) {
 			if (rowHover) {
-				this.setState({
-					rowHover: null
-				})
+				setRowHover(null)
+				// this.setState({
+				// 	rowHover: null
+				// })
 			}
-			this.timer = setTimeout(() => {
+			timer = setTimeout(() => {
 				getCollection(n.id).then(rs => {
 					if (rs.project.id) {
 						getProject(rs.project.id).then(rsp => {
 							rs.project = rsp
-							this.setState({ rowHover: e.target, hoverCollection: rs })
+							setRowHover(e.target)
+							setHoverCollection(rs)
+							// this.setState({ rowHover: e.target, hoverCollection: rs })
 						})
 					}
 					else {
-						this.setState({ rowHover: e.target, hoverCollection: rs })
+						setRowHover(e.target)
+						setHoverCollection(rs)
+						// this.setState({ rowHover: e.target, hoverCollection: rs })
 					}
 				})
 			}, hoverTime);
 		}
 	}
-	unsetTimeout = () => {
+	const unsetTimeout = () => {
 		// this.timer.forEach(e => clearTimeout(e))
-		clearTimeout(this.timer)
+		clearTimeout(timer)
 	}
-	unsetHover = () => {
-		this.setState({
-			rowHover: null
-		})
+	const unsetHover = () => {
+		setRowHover(null)
+		// this.setState({
+		// 	rowHover: null
+		// })
 	}
-	renderHover = () => {
-		return <CollectionHover anchorEl={this.state.rowHover} handleClose={this.unsetHover} collection={this.state.hoverCollection} />
+	const renderHover = () => {
+		return <CollectionHover anchorEl={rowHover} handleClose={unsetHover} collection={hoverCollection} />
 	}
 
-	renderIcon = (status) => {
-		const { classes, t } = this.props
+	const renderIcon = (status) => {
+		const { classes, t } = props
 		switch (status) {
 			case 0:
 				return <div title={t('devices.status.red')}>
@@ -111,123 +124,110 @@ class CollectionTable extends React.Component {
 		}
 	}
 
-	render() {
-		const { rowsPerPage, handleClick, classes, t, order, orderBy, data, selected, handleCheckboxClick } = this.props
-		const { page } = this.state
-		let emptyRows;
-		if (data)
-			emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage)
-		return (
-			<Fragment>
-				<div className={classes.tableWrapper} onMouseLeave={this.unsetHover}>
-					{this.renderHover()}
-					<Table className={classes.table} aria-labelledby='tableTitle'>
-						<TableHeader
-							numSelected={selected.length}
-							order={order}
-							orderBy={orderBy}
-							onSelectAllClick={this.handleSelectAllClick}
-							onRequestSort={this.handleRequestSort}
-							rowCount={data ? data.length : 0}
-							columnData={this.props.tableHead}
-							t={t}
-							classes={classes}
-							customColumn={
-								[{
-									id: 'activeDeviceStats.state',
-									label: <ItemG container title={t('collections.fields.status')} justify={'center'}>
-										<SignalWifi2Bar />
-									</ItemG>, checkbox: true
-								},
-								{
-									id: 'name', label: <Typography paragraph classes={{ root: classes.paragraphCell + ' ' + classes.headerCell }}>
-										{t('collections.fields.collection')}
-									</Typography>
-								}]
-							}
-						/>
-						<TableBody>
-							{data ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
-								const isSelected = this.isSelected(n.id);
-								return (
-									<TableRow
-										hover
-										onClick={handleClick(n.id)}
-										role='checkbox'
-										aria-checked={isSelected}
-										tabIndex={-1}
-										key={n.id}
-										selected={isSelected}
-										style={{ cursor: 'pointer' }}
-									>
-										<Hidden lgUp>
-											<TC checkbox content={<Checkbox checked={isSelected} onClick={e => handleCheckboxClick(e, n.id)} />} />
-											<TC checkbox content={this.renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
-											<TC content={
-												<ItemG container alignItems={'center'}>
-													<ItemG>
-														<Info noWrap paragraphCell={classes.noMargin}>
-															{n.name}
-														</Info>
-														<ItemG container>
-															<Caption noWrap className={classes.noMargin}>
-																{`${n.org ? n.org.name : ''} `}
-															</Caption>
-														</ItemG>
+	const { handleClick, classes, t, order, orderBy, data, selected, handleCheckboxClick } = props
+	let emptyRows;
+	if (data)
+		emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage)
+	return (
+		<Fragment>
+			<div className={classes.tableWrapper} onMouseLeave={unsetHover}>
+				{renderHover()}
+				<Table className={classes.table} aria-labelledby='tableTitle'>
+					<TableHeader
+						numSelected={selected.length}
+						order={order}
+						orderBy={orderBy}
+						onSelectAllClick={handleSelectAllClick}
+						onRequestSort={handleRequestSort}
+						rowCount={data ? data.length : 0}
+						columnData={props.tableHead}
+						t={t}
+						classes={classes}
+						customColumn={
+							[{
+								id: 'activeDeviceStats.state',
+								label: <ItemG container title={t('collections.fields.status')} justify={'center'}>
+									<SignalWifi2Bar />
+								</ItemG>, checkbox: true
+							},
+							{
+								id: 'name', label: <Typography paragraph classes={{ root: classes.paragraphCell + ' ' + classes.headerCell }}>
+									{t('collections.fields.collection')}
+								</Typography>
+							}]
+						}
+					/>
+					<TableBody>
+						{data ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
+							const isSelected = isSelectedFunc(n.id);
+							return (
+								<TableRow
+									hover
+									onClick={handleClick(n.id)}
+									role='checkbox'
+									aria-checked={isSelected}
+									tabIndex={-1}
+									key={n.id}
+									selected={isSelected}
+									style={{ cursor: 'pointer' }}
+								>
+									<Hidden lgUp>
+										<TC checkbox content={<Checkbox checked={isSelected} onClick={e => handleCheckboxClick(e, n.id)} />} />
+										<TC checkbox content={renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
+										<TC content={
+											<ItemG container alignItems={'center'}>
+												<ItemG>
+													<Info noWrap paragraphCell={classes.noMargin}>
+														{n.name}
+													</Info>
+													<ItemG container>
+														<Caption noWrap className={classes.noMargin}>
+															{`${n.org ? n.org.name : ''} `}
+														</Caption>
 													</ItemG>
 												</ItemG>
-											}
-											/>
-										</Hidden>
-										<Hidden mdDown >
-											<TC checkbox content={<Checkbox checked={isSelected} onClick={e => handleCheckboxClick(e, n.id)} />} />
-											<TC label={n.id} />
-											<TC FirstC label={n.name}
-												onMouseEnter={e => { this.setHover(e, n) }}
-												onMouseLeave={this.unsetTimeout}
-											/>
-											<TC content={this.renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
-											<TC label={dateFormatter(n.created)} />
-											<TC label={n.devices ? n.devices[0] ? dateFormatter(n.devices[0].start) : '' : ''} />
-											<TC label={n.org ? n.org.name : ''} />
-										</Hidden>
-									</TableRow>
-								)
-							}) : null}
-							{emptyRows > 0 && (
-								<TableRow style={{ height: 49 }}>
-									<TableCell colSpan={8} />
+											</ItemG>
+										}
+										/>
+									</Hidden>
+									<Hidden mdDown >
+										<TC checkbox content={<Checkbox checked={isSelected} onClick={e => handleCheckboxClick(e, n.id)} />} />
+										<TC label={n.id} />
+										<TC FirstC label={n.name}
+											onMouseEnter={e => { setHover(e, n) }}
+											onMouseLeave={unsetTimeout}
+										/>
+										<TC content={renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
+										<TC label={dateFormatter(n.created)} />
+										<TC label={n.devices ? n.devices[0] ? dateFormatter(n.devices[0].start) : '' : ''} />
+										<TC label={n.org ? n.org.name : ''} />
+									</Hidden>
 								</TableRow>
-							)}
-						</TableBody>
-					</Table>
-				</div>
-				<TP
-					count={data ? data.length : 0}
-					classes={classes}
-					rowsPerPage={rowsPerPage}
-					page={page}
-					t={t}
-					handleChangePage={this.handleChangePage}
-				/>
-			</Fragment>
+							)
+						}) : null}
+						{emptyRows > 0 && (
+							<TableRow style={{ height: 49 }}>
+								<TableCell colSpan={8} />
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			<TP
+				count={data ? data.length : 0}
+				classes={classes}
+				rowsPerPage={rowsPerPage}
+				page={page}
+				t={t}
+				handleChangePage={handleChangePage}
+			/>
+		</Fragment>
 
-		)
-	}
-}
-const mapStateToProps = (state) => ({
-	rowsPerPage: state.appState.trp ? state.appState.trp : state.settings.trp,
-	language: state.localization.language,
-	accessLevel: state.settings.user.privileges,
-	hoverTime: state.settings.hoverTime
-})
-
-const mapDispatchToProps = {
-
+	)
 }
 
 CollectionTable.propTypes = {
 	classes: PropTypes.object.isRequired,
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withStyles(devicetableStyles, { withTheme: true })(CollectionTable)))
+export default withRouter(withStyles(devicetableStyles, { withTheme: true })(CollectionTable))
