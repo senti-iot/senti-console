5bcda442d61a6efc3d8ceb2b28deab011ccf8279
SromPavel <1074227@ucn.dk>
2020-01-29 14:37:10 +0100

    Date(Time)Picker.js => func components

--- src/components/CustomInput/DatePicker.js
+++ src/components/CustomInput/DatePicker.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { KeyboardArrowRight, KeyboardArrowLeft } from 'variables/icons';
 import { DatePicker as MuiDatePicker } from 'material-ui-pickers';
 
-class DatePicker extends Component {
-	render() {
-		const { className, value, label, onChange, disableFuture, error } = this.props
-		return (
-			<MuiDatePicker
-				style={{ maxWidth: 230 }}
-				autoOk
-				margin={'normal'}
-				variant={'outlined'}
-				label={label}
-				clearable
-				mask={value =>
-					value ? [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/] : []
-				  }
-				// format={'ll'}
-				error={error}
-				format={ "DD/MM/YYYY"}
-				placeholder="DD/MM/YYYY"
-				value={value}
-				keyboard
-				className={className}
-				onChange={onChange}
-				animateYearScrolling={false}
-				color='primary'
-				disableFuture={disableFuture}
-				rightArrowIcon={<KeyboardArrowRight />}
-				leftArrowIcon={<KeyboardArrowLeft />}
-			/>
-		)
-	}
-}
+const DatePicker = ({ className, value, label, onChange, disableFuture, error }) => (
+	<MuiDatePicker
+		style={{ maxWidth: 230 }}
+		autoOk
+		margin={'normal'}
+		variant={'outlined'}
+		label={label}
+		clearable
+		mask={value =>
+			value ? [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/] : []
+		}
+		// format={'ll'}
+		error={error}
+		format={"DD/MM/YYYY"}
+		placeholder="DD/MM/YYYY"
+		value={value}
+		keyboard
+		className={className}
+		onChange={onChange}
+		animateYearScrolling={false}
+		color='primary'
+		disableFuture={disableFuture}
+		rightArrowIcon={<KeyboardArrowRight />}
+		leftArrowIcon={<KeyboardArrowLeft />}
+	/>
+)
 
 export default DatePicker
