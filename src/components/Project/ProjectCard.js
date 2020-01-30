0de08b66724896f709834e25ad99b5c384f14245
SromPavel <1074227@ucn.dk>
2020-01-30 15:33:10 +0100

    ProjectCard.js => hooks

--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -1,78 +1,80 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { IconButton, Menu, MenuItem, withStyles, Button, Typography } from '@material-ui/core'
 import { ItemGrid, SmallCard } from 'components'
 import regularCardStyle from 'assets/jss/material-dashboard-react/regularCardStyle'
 import { MoreVert, Edit, /* PictureAsPdf, Devices, Delete, */ LibraryBooks } from 'variables/icons'
 import { withRouter } from 'react-router-dom'
+import { useLocalization } from 'hooks'
 
-class ProjectCard extends Component {
-	constructor(props) {
-	  super(props)
-	
-	  this.state = {
-		 actionAnchor: null,
-		 img: null
-	  }
+const ProjectCard = props => {
+	const t = useLocalization()
+	const [actionAnchor, setActionAnchor] = useState(null)
+	const [img, /* setImg */] = useState(null)
+	// constructor(props) {
+	//   super(props)
+
+	//   this.state = {
+	// 	 actionAnchor: null,
+	// 	 img: null
+	//   }
+	// }
+
+	const handleOpenActionsDetails = event => {
+		setActionAnchor(event.currentTarget)
+		// this.setState({ actionAnchor: event.currentTarget });
 	}
 
-	handleOpenActionsDetails = event => {
-		this.setState({ actionAnchor: event.currentTarget });
+	const handleCloseActionsDetails = () => {
+		setActionAnchor(null)
+		// this.setState({ actionAnchor: null });
 	}
 
-	handleCloseActionsDetails = () => {
-		this.setState({ actionAnchor: null });
-	}
+	// handleEdit = () => {
+	// }
 
-	handleEdit = () => {
-	}
+	// handleDeleteProject = () => {
+	// }
+	const { p, classes } = props
 
-	handleDeleteProject = () => {
-	}
-
-	render() {
-		const { p, classes, t } = this.props
-		const { actionAnchor } = this.state
-		return (
-			
-			<SmallCard
-				avatar={<LibraryBooks />}
-				key={p.id}
-				title={p.title}
-				img={this.state.img}
-				topAction={
-					<ItemGrid noMargin noPadding>
-						<IconButton
-							aria-label='More'
-							aria-owns={actionAnchor ? 'long-menu' : null}
-							aria-haspopup='true'
-							onClick={this.handleOpenActionsDetails}>
-							<MoreVert />
-						</IconButton>
-						<Menu
-							id='long-menu'
-							anchorEl={actionAnchor}
-							open={Boolean(actionAnchor)}
-							onClose={this.handleCloseActionsDetails}
-							PaperProps={{
-								style: {
-									minWidth: 200
-								}
-							}}>
-							<MenuItem onClick={() => this.props.history.push(`/project/${p.id}/edit`)}>
-								<Edit className={classes.leftIcon} />{t('menus.edit')}									
-							</MenuItem>
-						</Menu>
-					</ItemGrid>
-				}
-				content={<Typography>{p.description}</Typography>}
-				rightActions={
-					<Button variant={'text'} color={'primary'} onClick={() => this.props.history.push(`/project/${p.id}`)}>
-						{t('menus.seeMore')}
-					</Button>
-				}
-			/>
-		)
-	}
+	return (
+		<SmallCard
+			avatar={<LibraryBooks />}
+			key={p.id}
+			title={p.title}
+			img={img}
+			topAction={
+				<ItemGrid noMargin noPadding>
+					<IconButton
+						aria-label='More'
+						aria-owns={actionAnchor ? 'long-menu' : null}
+						aria-haspopup='true'
+						onClick={handleOpenActionsDetails}>
+						<MoreVert />
+					</IconButton>
+					<Menu
+						id='long-menu'
+						anchorEl={actionAnchor}
+						open={Boolean(actionAnchor)}
+						onClose={handleCloseActionsDetails}
+						PaperProps={{
+							style: {
+								minWidth: 200
+							}
+						}}>
+						<MenuItem onClick={() => props.history.push(`/project/${p.id}/edit`)}>
+							<Edit className={classes.leftIcon} />{t('menus.edit')}
+						</MenuItem>
+					</Menu>
+				</ItemGrid>
+			}
+			content={<Typography>{p.description}</Typography>}
+			rightActions={
+				<Button variant={'text'} color={'primary'} onClick={() => props.history.push(`/project/${p.id}`)}>
+					{t('menus.seeMore')}
+				</Button>
+			}
+		/>
+	)
 }
 
 export default withRouter(withStyles(regularCardStyle)(ProjectCard))
