edb04f7e945c90fa1b5a835b668d4bccb624eff1
SromPavel <1074227@ucn.dk>
2020-01-30 12:07:34 +0100

    EditDeviceDetails.js => hooks

--- src/components/Devices/EditDeviceDetails.js
+++ src/components/Devices/EditDeviceDetails.js
@@ -1,96 +1,140 @@
 import { Button, Collapse, Grid, Paper, withStyles, Fade } from '@material-ui/core';
 import createprojectStyles from 'assets/jss/components/projects/createprojectStyles';
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import { updateDevice, getGeoByAddress, getAddress } from 'variables/dataDevices';
 import { CircularLoader, GridContainer, ItemGrid, TextF, AddressInput } from 'components';
 import DSelect from 'components/CustomInput/DSelect';
 import { isFav, updateFav } from 'redux/favorites';
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { getDeviceLS, getDevices } from 'redux/data';
+import { useLocalization, useSnackbar } from 'hooks';
 
-class EditDeviceDetails extends Component {
-	constructor(props) {
-		super(props)
 
-		this.state = {
-			loading: true,
-			updating: false,
-			updated: false,
-			device: null
-		}
-		let prevURL = props.location.prevURL ? props.location.prevURL : '/devices/list'
-		props.setHeader({ id: 'devices.editDetailsTitle', options: { deviceId: props.match.params.id } }, true, prevURL, 'devices')
-	}
-	keyHandler = (e) => {
+// const mapStateToProps = (state) => ({
+// 	device: state.data.device,
+// 	loading: !state.data.gotDevice
+// })
+// const mapDispatchToProps = (dispatch) => ({
+// 	isFav: (favObj) => dispatch(isFav(favObj)),
+// 	updateFav: (favObj) => dispatch(updateFav(favObj)),
+// 	getDevice: async id => dispatch(await getDeviceLS(id)),
+// 	getDevices: reload => dispatch(getDevices(reload))
+// })
+
+const EditDeviceDetails = props => {
+	const t = useLocalization()
+	const s = useSnackbar().s
+	const dispatch = useDispatch()
+	const device = useSelector(store => store.data.device)
+	const loading = useSelector(store => !store.data.gotDevice)
+
+	// const [stateLoading, setStateLoading] = useState(true)
+	const [updating, setUpdating] = useState(false)
+	const [updated, setUpdated] = useState(false)
+	const [stateDevice, setStateDevice] = useState(null)
+	// constructor(props) {
+	// 	super(props)
+
+	// 	this.state = {
+	// 		loading: true,
+	// 		updating: false,
+	// 		updated: false,
+	// 		device: null
+	// 	}
+	let prevURL = props.location.prevURL ? props.location.prevURL : '/devices/list'
+	props.setHeader({ id: 'devices.editDetailsTitle', options: { deviceId: props.match.params.id } }, true, prevURL, 'devices')
+
+	const keyHandler = (e) => {
 		if (e.key === 'Escape') {
-			this.goToDevice()
-		}
-	}
-	componentDidUpdate = (prevProps, prevState) => {
-		const { device } = this.props
-		if ((!prevProps.device && device !== prevProps.device) || (this.state.device === null && device)) {
-			this.props.setBC('editdevicedetails', device.name, device.id)
-			this.setState({
-				device: device,
-			})
+			goToDevice()
 		}
 	}
 
-	componentDidMount = async () => {
-		let id = this.props.match.params.id
-		const { getDevice } = this.props
-		await getDevice(id)
-		window.addEventListener('keydown', this.keyHandler, false)
-	}
-	componentWillUnmount = () => {
-		window.removeEventListener('keydown', this.keyHandler, false)
-		clearTimeout(this.timer);
-	}
-	setMapCoords = (data) => {
+	useEffect(() => {
+		props.setBC('editdevicedetails', device.name, device.id)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [device, stateDevice])
+	// const componentDidUpdate = (prevProps, prevState) => {
+	// 	if ((!prevProps.device && device !== prevProps.device) || (this.state.device === null && device)) {
+	// 		this.props.setBC('editdevicedetails', device.name, device.id)
+
+	// 		// this.setState({
+	// 		// 	device: device,
+	// 		// })
+	// 	}
+	// }
+
+	useEffect(() => {
+		const asyncFunc = async () => {
+			let id = props.match.params.id
+			dispatch(await getDeviceLS(id))
+			window.addEventListener('keydown', keyHandler, false)
+		}
+		asyncFunc()
+
+		return () => {
+			window.removeEventListener('keydown', keyHandler, false)
+			// clearTimeout(timer);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+	// const componentDidMount = async () => {
+	// 	let id = props.match.params.id
+	// 	const { getDevice } = this.props
+	// 	await getDevice(id)
+	// 	window.addEventListener('keydown', keyHandler, false)
+	// }
+	// const componentWillUnmount = () => {
+	// 	window.removeEventListener('keydown', keyHandler, false)
+	// 	clearTimeout(timer);
+	// }
+	const setMapCoords = (data) => {
 		let coords = { lat: data.adgangsadresse.vejpunkt.koordinater[1], long: data.adgangsadresse.vejpunkt.koordinater[0] }
 		if (coords) {
-			this.setState({
-				device: {
-					...this.state.device,
-					lat: coords.lat,
-					long: coords.long,
-				}
-			})
+			setStateDevice({ ...stateDevice, lat: coords.lat, lang: coords.lang })
+			// this.setState({
+			// 	device: {
+			// 		...stateDevice,
+			// 		lat: coords.lat,
+			// 		long: coords.long,
+			// 	}
+			// })
 		}
 	}
-	getLatLng = async (suggestion) => {
+	const getLatLng = async (suggestion) => {
 		let data
 		if (suggestion.id) {
 			data = await getGeoByAddress(suggestion.id)
 			if (data) {
-				return this.setMapCoords(data)
+				return setMapCoords(data)
 			}
 		}
 		else {
-			data = await getAddress(this.state.device.address)
-			return this.setMapCoords(data)
+			data = await getAddress(stateDevice.address)
+			return setMapCoords(data)
 		}
 	}
-	handleSetAddress = (address) => {
-		this.setState({
-			device: {
-				...this.state.device,
-				address
-			}
-		})
+	const handleSetAddress = (address) => {
+		setStateDevice({ ...stateDevice, address })
+		// this.setState({
+		// 	device: {
+		// 		...this.state.device,
+		// 		address
+		// 	}
+		// })
 	}
-	handleInput = (input) => e => {
+	const handleInput = (input) => e => {
 		if (e.preventDefault)
 			e.preventDefault()
-		this.setState({
-			device: {
-				...this.state.device,
-				[input]: e.target.value
-			}
-		})
+		setStateDevice({ ...stateDevice, [input]: e.target.value })
+		// this.setState({
+		// 	device: {
+		// 		...stateDevice,
+		// 		[input]: e.target.value
+		// 	}
+		// })
 	}
-	LocationTypes = () => {
-		const { t } = this.props
+	const LocationTypes = () => {
 		return [
 			{ value: 0, label: t('devices.locationTypes.unspecified') },
 			{ value: 1, label: t('devices.locationTypes.pedStreet') },
@@ -103,133 +147,125 @@ class EditDeviceDetails extends Component {
 			{ value: 8, label: t('devices.locationTypes.port') },
 			{ value: 9, label: t('devices.locationTypes.office') }]
 	}
-	handleUpdateFav = () => {
-		const { isFav, updateFav } = this.props
-		const { device } = this.state
+	const handleUpdateFav = () => {
+		// const { isFav, updateFav } = this.props
+		// const { device } = this.state
 		let favObj = {
-			id: device.id,
-			name: device.name,
+			id: stateDevice.id,
+			name: stateDevice.name,
 			type: 'device',
-			path: `/device/${device.id}`
+			path: `/device/${stateDevice.id}`
 		}
-		if (isFav(favObj)) {
-			updateFav(favObj)
+		if (dispatch(isFav(favObj))) {
+			dispatch(updateFav(favObj))
 		}
-		this.setState({ updated: true, updating: false })
-		this.props.s('snackbars.deviceUpdated', { device: this.state.device.id })
-		this.goToDevice()
+		setUpdated(true)
+		setUpdating(false)
+		// this.setState({ updated: true, updating: false })
+		s('snackbars.deviceUpdated', { device: stateDevice.id })
+		goToDevice()
 	}
-	handleUpdateDevice = async () => {
-		const { device } = this.state
-		this.setState({ updating: true })
+	const handleUpdateDevice = async () => {
+		// const { device } = this.state
+		setUpdating(true)
+		// this.setState({ updating: true })
 		let updateD = {
 			...device,
 			project: {
 				id: 0
 			}
 		}
-		await updateDevice(updateD).then(rs => rs ? this.handleUpdateFav() : null)
+		await updateDevice(updateD).then(rs => rs ? handleUpdateFav() : null)
 	}
 
-	goToDevice = () => {
-		const { location, history } = this.props
-		this.props.getDevice(this.state.device.id)
-		this.props.getDevices(true)
-		history.push(location.prevURL ? location.prevURL : `/device/${this.props.match.params.id}`)
+	const goToDevice = async () => {
+		const { location, history } = props
+		dispatch(await getDeviceLS(stateDevice.id))
+		dispatch(getDevices(true))
+		history.push(location.prevURL ? location.prevURL : `/device/${props.match.params.id}`)
 	}
-	render() {
-		const { classes, t, loading } = this.props
-		const { device } = this.state
-		return !loading && device ? (
-			<GridContainer>
-				<Fade in={true}>
-					<Paper className={classes.paper}>
-						<form className={classes.form}>
-							<Grid container>
-								<ItemGrid xs={12}>
-									<TextF
-										id={'name'}
-										label={t('devices.fields.name')}
-										onChange={this.handleInput('name')}
-										value={device.name}
-										autoFocus
-									/>
-								</ItemGrid>
-								<ItemGrid xs={12}>
-									<DSelect
-										label={t('devices.fields.locType')}
-										value={device.locationType}
-										onChange={this.handleInput('locationType')}
-										menuItems={this.LocationTypes()}
-									/>
-								</ItemGrid>
-								<ItemGrid xs={12}>
-									<TextF
-										id={'description'}
-										label={t('devices.fields.description')}
-										multiline
-										rows={4}
-										onChange={this.handleInput('description')}
-										value={device.description}
 
-									/>
-								</ItemGrid>
-								<ItemGrid xs={12}>
-									<AddressInput
-										onBlur={this.getLatLng}
-										handleSuggestionSelected={this.getLatLng}
-										value={device.address}
-										handleChange={this.handleSetAddress} />
-								</ItemGrid>
-								<ItemGrid xs={12} container justify={'center'}>
-									<Collapse in={this.state.updating} timeout={100} unmountOnExit>
-										<CircularLoader fill />
-									</Collapse>
-								</ItemGrid>
-								<ItemGrid container style={{ margin: 16 }}>
-									<div className={classes.wrapper}>
-										<Button
-											variant='outlined'
-											onClick={this.goToDevice}
-											className={classes.redButton}
-										>
-											{t('actions.cancel')}
-										</Button>
-									</div>
-									<div className={classes.wrapper}>
-										<Button
-											variant='outlined'
-											color='primary'
-											disabled={this.state.updating || this.state.updated}
-											onClick={this.handleUpdateDevice}
-										>
-											{this.state.updated ?
-												<Fragment>
-													{t('snackbars.redirect')}
-												</Fragment> :
-												<Fragment>
-													{t('actions.save')}
-												</Fragment>}
-										</Button>
-									</div>
-								</ItemGrid>
-							</Grid>
-						</form>
-					</Paper>
-				</Fade>
-			</GridContainer>
-		) : <CircularLoader />
-	}
+	const { classes } = props
+	// const { device } = this.state
+	return !loading && device ? (
+		<GridContainer>
+			<Fade in={true}>
+				<Paper className={classes.paper}>
+					<form className={classes.form}>
+						<Grid container>
+							<ItemGrid xs={12}>
+								<TextF
+									id={'name'}
+									label={t('devices.fields.name')}
+									onChange={handleInput('name')}
+									value={device.name}
+									autoFocus
+								/>
+							</ItemGrid>
+							<ItemGrid xs={12}>
+								<DSelect
+									label={t('devices.fields.locType')}
+									value={device.locationType}
+									onChange={handleInput('locationType')}
+									menuItems={LocationTypes()}
+								/>
+							</ItemGrid>
+							<ItemGrid xs={12}>
+								<TextF
+									id={'description'}
+									label={t('devices.fields.description')}
+									multiline
+									rows={4}
+									onChange={handleInput('description')}
+									value={device.description}
+
+								/>
+							</ItemGrid>
+							<ItemGrid xs={12}>
+								<AddressInput
+									onBlur={getLatLng}
+									handleSuggestionSelected={getLatLng}
+									value={device.address}
+									handleChange={handleSetAddress} />
+							</ItemGrid>
+							<ItemGrid xs={12} container justify={'center'}>
+								<Collapse in={updating} timeout={100} unmountOnExit>
+									<CircularLoader fill />
+								</Collapse>
+							</ItemGrid>
+							<ItemGrid container style={{ margin: 16 }}>
+								<div className={classes.wrapper}>
+									<Button
+										variant='outlined'
+										onClick={goToDevice}
+										className={classes.redButton}
+									>
+										{t('actions.cancel')}
+									</Button>
+								</div>
+								<div className={classes.wrapper}>
+									<Button
+										variant='outlined'
+										color='primary'
+										disabled={updating || updated /* this.state.updated */}
+										onClick={handleUpdateDevice}
+									>
+										{/* this.state. */updated ?
+											<Fragment>
+												{t('snackbars.redirect')}
+											</Fragment> :
+											<Fragment>
+												{t('actions.save')}
+											</Fragment>}
+									</Button>
+								</div>
+							</ItemGrid>
+						</Grid>
+					</form>
+				</Paper>
+			</Fade>
+		</GridContainer>
+	) : <CircularLoader />
 }
 
-const mapStateToProps = (state) => ({
-	device: state.data.device,
-	loading: !state.data.gotDevice
-})
-const mapDispatchToProps = (dispatch) => ({
-	isFav: (favObj) => dispatch(isFav(favObj)),
-	updateFav: (favObj) => dispatch(updateFav(favObj)),
-	getDevice: async id => dispatch(await getDeviceLS(id)),
-	getDevices: reload => dispatch(getDevices(reload))
-})
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(createprojectStyles)(EditDeviceDetails))
+export default withStyles(createprojectStyles)(EditDeviceDetails)
