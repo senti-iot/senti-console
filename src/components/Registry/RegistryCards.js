8b59dc773819dc90a8d915af48a52d7bcf25db1e
SromPavel <1074227@ucn.dk>
2020-01-31 10:34:41 +0100

    RegistryCards.js => hooks

--- src/components/Registry/RegistryCards.js
+++ src/components/Registry/RegistryCards.js
@@ -1,56 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import RegistryCard from './RegistryCard';
 import { ItemG, GridContainer } from 'components';
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import devicetableStyles from 'assets/jss/components/devices/devicetableStyles';
 import { withStyles } from '@material-ui/core';
 import CP from 'components/Table/CP';
+import { useLocalization } from 'hooks';
 
-class RegistryCards extends Component {
-	constructor(props) {
-	  super(props)
-	
-		this.state = {
-			page: 0
-	  }
-	}
-	
-	static propTypes = {
-		projects: PropTypes.array,
-	}
-	
-	handleChangePage = (event, page) => {
-		this.setState({ page });
+// const mapStateToProps = (state) => ({
+// 	rowsPerPage: state.appState.CPP
+// })
+
+const RegistryCards = props => {
+	const t = useLocalization()
+	const rowsPerPage = useSelector(store => store.appState.CPP)
+	const [page, setPage] = useState(0)
+	// constructor(props) {
+	//   super(props)
+
+	// 	this.state = {
+	// 		page: 0
+	//   }
+	// }
+
+	const handleChangePage = (event, newpage) => {
+		setPage(newpage)
+		// this.setState({ page });
 	}
 
-	render() {
-		const { registries, t,  rowsPerPage, classes } = this.props
-		const { page } = this.state
-		return (
-			<GridContainer spacing={2}>			
-				{registries ? registries.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((p, i) =>
-					<ItemG container key={i} justify={'center'} xs={12} sm={6} md={4}><RegistryCard t={t} key={p.id} p={p} /></ItemG>
-				) : null}
-				<ItemG xs={12}>
-					<CP
-						count={registries ? registries.length : 0}
-						classes={classes}
-						page={page}
-						t={t}
-						handleChangePage={this.handleChangePage}
-					/>
-				</ItemG>
-			</GridContainer>
-
-		)
-	}
-}
-const mapStateToProps = (state) => ({
-	rowsPerPage: state.appState.CPP
-})
-
-const mapDispatchToProps = {
+	const { registries, classes } = props
+	return (
+		<GridContainer spacing={2}>
+			{registries ? registries.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((p, i) =>
+				<ItemG container key={i} justify={'center'} xs={12} sm={6} md={4}><RegistryCard t={t} key={p.id} p={p} /></ItemG>
+			) : null}
+			<ItemG xs={12}>
+				<CP
+					count={registries ? registries.length : 0}
+					classes={classes}
+					page={page}
+					t={t}
+					handleChangePage={handleChangePage}
+				/>
+			</ItemG>
+		</GridContainer>
+	)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(devicetableStyles, { withTheme: true })(RegistryCards))
+RegistryCards.propTypes = {
+	projects: PropTypes.array
+}
+
+export default withStyles(devicetableStyles, { withTheme: true })(RegistryCards)
