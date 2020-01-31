417b47f2f07c00987cd1aeeb6b8a6b615646e0ea
SromPavel <1074227@ucn.dk>
2020-01-31 11:34:03 +0100

    TableData.js => hooks

--- src/components/Table/TableData.js
+++ src/components/Table/TableData.js
@@ -1,107 +1,106 @@
-import React, { Component, Fragment } from 'react'
+import React, { useState, Fragment } from 'react'
 import withLocalization from 'components/Localization/T';
 import { withStyles, Table, TableBody, TableRow, Checkbox } from '@material-ui/core';
 import TableHeader from './TableHeader';
 import TC from './TC';
 import TP from './TP';
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import devicetableStyles from 'assets/jss/components/devices/devicetableStyles';
+import { useLocalization } from 'hooks'
 
-class TableData extends Component {
-	constructor(props) {
-	  super(props)
-	
-	  this.state = {
-		 page: 0
-	  }
-	}
-	
-	headers = () => {
-		const { t } = this.props
+// const mapStateToProps = (state) => ({
+// 	rowsPerPage: state.appState.trp ? state.appState.trp : state.settings.trp,
+// 	accessLevel: state.settings.user.privileges
+// })
+
+const TableData = props => {
+	const t = useLocalization()
+	const rowsPerPage = useSelector(state => state.appState.trp ? state.appState.trp : state.settings.trp)
+	// const accessLevel = useSelector(state => state.settings.user.privileges)
+	const [page, setPage] = useState(0)
+	// constructor(props) {
+	//   super(props)
+
+	//   this.state = {
+	// 	 page: 0
+	//   }
+	// }
+
+	const headers = () => {
 		return [
 			{ id: 'id', label: t('charts.fields.id') },
 			{ id: 'date', label: t('charts.fields.interval') },
 			{ id: 'count', label: t('charts.fields.count') }
 		]
 	}
-	handleRequestSort = (event, property) => {
-		this.props.handleRequestSort(event, property)
+	const handleRequestSort = (event, property) => {
+		props.handleRequestSort(event, property)
 	}
 
-	handleChangePage = (event, page) => {
-		this.setState({ page });
+	const handleChangePage = (event, newpage) => {
+		setPage(newpage)
+		// this.setState({ page });
 	};
 
-	isSelected = id => {
-		return this.props.selected.indexOf(id) !== -1;
+	const isSelectedFunc = id => {
+		return props.selected.indexOf(id) !== -1;
 	}
 
-	render() {
-		const { page } = this.state
-		const { classes, t, data } = this.props
-		const { selected, order, orderBy, handleClick, handleCheckboxClick, handleSelectAllClick, rowsPerPage } = this.props
-		return (
-			<Fragment>
-				<div className={classes.tableWrapper}>
-					<Table className={classes.table}>
-						<TableHeader
-							numSelected={selected.length}
-							order={order}
-							orderBy={orderBy}
-							onSelectAllClick={handleSelectAllClick}
-							onRequestSort={this.handleRequestSort}
-							rowCount={data ? data.length : 0}
-							columnData={this.headers()}
-							classes={classes}
-						/>
-						<TableBody>
-							{data ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((n, i) => {
-								const isSelected = this.isSelected(n.id);
-								return (
-								
-									<TableRow
-										hover
-										role='checkbox'
-										aria-checked={isSelected}
-										onClick={handleClick}
-										tabIndex={-1}
-										key={i}
-										selected={isSelected}
-										style={{ cursor: 'pointer' }}
-									>
-										<TC checkbox content={<Checkbox checked={isSelected} onClick={e => handleCheckboxClick(e, n.id)} />} /> 
-										<TC label={n.id} />
-										<TC label={n.interval} />
-										<TC label={n.count} />
-											
-									</TableRow>
-								);
-							}) : alert('NoData')}
+	const { classes, data } = props
+	const { selected, order, orderBy, handleClick, handleCheckboxClick, handleSelectAllClick } = props
+	return (
+		<Fragment>
+			<div className={classes.tableWrapper}>
+				<Table className={classes.table}>
+					<TableHeader
+						numSelected={selected.length}
+						order={order}
+						orderBy={orderBy}
+						onSelectAllClick={handleSelectAllClick}
+						onRequestSort={handleRequestSort}
+						rowCount={data ? data.length : 0}
+						columnData={headers()}
+						classes={classes}
+					/>
+					<TableBody>
+						{data ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((n, i) => {
+							const isSelected = isSelectedFunc(n.id);
+							return (
 
-						</TableBody>
+								<TableRow
+									hover
+									role='checkbox'
+									aria-checked={isSelected}
+									onClick={handleClick}
+									tabIndex={-1}
+									key={i}
+									selected={isSelected}
+									style={{ cursor: 'pointer' }}
+								>
+									<TC checkbox content={<Checkbox checked={isSelected} onClick={e => handleCheckboxClick(e, n.id)} />} />
+									<TC label={n.id} />
+									<TC label={n.interval} />
+									<TC label={n.count} />
 
-							
-					</Table>
-				</div>
-				<TP
-					count={data ? data.length : 0}
-					classes={classes}
-					// rowsPerPage={rowsPerPage}
-					page={page}
-					t={t}
-					handleChangePage={this.handleChangePage}
-				/>
-			</Fragment>
-		)
-	}
-}
-const mapStateToProps = (state) => ({
-	rowsPerPage: state.appState.trp ? state.appState.trp : state.settings.trp,
-	accessLevel: state.settings.user.privileges
-})
+								</TableRow>
+							);
+						}) : alert('NoData')}
 
-const mapDispatchToProps = {
+					</TableBody>
 
+
+				</Table>
+			</div>
+			<TP
+				count={data ? data.length : 0}
+				classes={classes}
+				// rowsPerPage={rowsPerPage}
+				page={page}
+				t={t}
+				handleChangePage={handleChangePage}
+			/>
+		</Fragment>
+	)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withLocalization()(withStyles(devicetableStyles)(TableData)))
+export default withLocalization()(withStyles(devicetableStyles)(TableData))
