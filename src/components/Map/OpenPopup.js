c2aee4ac2f764937f107ba1ca6dd72363dfd451b
SromPavel <1074227@ucn.dk>
2020-02-03 15:05:36 +0100

    OpenPopup.js => hooks

--- src/components/Map/OpenPopup.js
+++ src/components/Map/OpenPopup.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Paper, Button, withStyles, Typography, Collapse } from '@material-ui/core';
 import { ItemG, Caption, Info, WeatherIcon, T } from 'components';
 import { Link } from 'react-router-dom';
@@ -7,8 +7,9 @@ import { red, green, yellow, teal } from '@material-ui/core/colors'
 import { SignalWifi2Bar, SignalWifi2BarLock } from 'variables/icons';
 import moment from 'moment'
 // import { getWeather } from 'variables/dataDevices';
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { getWeather } from 'redux/weather';
+import { useLocalization } from 'hooks';
 
 const styles = theme => ({
 	paper: {
@@ -31,17 +32,39 @@ const styles = theme => ({
 	},
 })
 
-class OpenPopup extends Component {
-	constructor(props) {
-	  super(props)
-	
-	  this.state = {
-		 weather: null
-	  }
-	}
-	
-	renderIcon = (status) => {
-		const { classes, t } = this.props
+// const mapStateToProps = (state) => ({
+// 	lang: state.settings.language,
+// 	heatData: state.dateTime.heatData,
+// 	weather: state.weather.weather,
+// 	loadingWeather: state.weather.loading
+// })
+
+
+// const mapDispatchToProps = dispatch => ({
+// 	getWeather: async (device, date, lang) => dispatch(await getWeather(device, date, lang))
+// })
+
+const OpenPopup = props => {
+	const t = useLocalization()
+	const dispatch = useDispatch()
+	// const lang = useSelector(state => state.settings.language)
+	// const heatData = useSelector(state => state.dateTime.heatData)
+	const weather = useSelector(state => state.weather.weather)
+	// const loadingWeather = useSelector(state => state.weather.loading)
+
+	const [stateWeather, setStateWeather] = useState(null)
+
+	// constructor(props) {
+	//   super(props)
+
+	//   this.state = {
+	// 	 weather: null
+	//   }
+	// }
+
+	// eslint-disable-next-line no-unused-vars
+	const renderIcon = (status) => {
+		const { classes } = props
 		switch (status) {
 			case 1:
 				return <div title={t('devices.status.yellow')}><SignalWifi2Bar className={classes.yellowSignal} /></div>
@@ -53,106 +76,102 @@ class OpenPopup extends Component {
 				return <div title={t('devices.status.red')}> <SignalWifi2BarLock className={classes.redSignal} /></div>
 		}
 	}
-	getWeather = async () => { 
-		const { m } = this.props
+	const getWeatherFunc = async () => {
+		const { m } = props
 		if (m.lat && m.long)
-			await this.props.getWeather({ lat: m.lat, long: m.long },
-				moment(), this.props.lang).then((rs) => {
-				this.setState({
-					weather: this.props.weather
+			dispatch(await getWeather({ lat: m.lat, long: m.long }, moment(), props.lang))
+
+				// await this.props.getWeather({ lat: m.lat, long: m.long }, moment(), this.props.lang)
+				.then((rs) => {
+					setStateWeather(weather)
+					// this.setState({
+					// 	weather: this.props.weather
+					// })
 				})
-			})
 
-	 }
-	componentDidMount =async () => {
-		await this.getWeather()
 	}
-	render() {
-		const { classes, m, t, noSeeMore, heatMap, dontShow } = this.props
-		const { weather } = this.state
-		return !dontShow ? <Paper className={classes.paper}>
-			<ItemG container>
-				<ItemG xs={10} container direction={'column'}>
-					<Typography variant={'h5'}>{m.name ? m.name : m.id}</Typography>
-					<Typography variant={'body1'}>{`(${m.id})`}</Typography>
-				</ItemG>
-				<ItemG xs={2} container>
-					<ItemG container justify={'flex-end'}>
-						{this.renderIcon(m.liveStatus)}
-					</ItemG>
-				</ItemG>
-				<ItemG xs={12}>
-					<Typography variant={'body1'}> {`${t('charts.fields.date')}: ${moment().format('lll')}`}</Typography>
-				</ItemG>
-			
-				<ItemG xs={12}>
-					<Caption>{t('devices.fields.address')}</Caption>
-					<Info>{m.address ? m.address : t('devices.noAddress')}</Info>
-				</ItemG>
-				<ItemG xs={6}>
-					<Caption>{t('devices.fields.temp')}</Caption>
-					<Info>{m.temperature ? `${m.temperature}\u2103` : `-\u2103`}</Info>
-				</ItemG>
-				{heatMap ? <ItemG xs={6}>
-					<Caption>{t('devices.simpleList.totalCount')}</Caption>
-					<Info>{m.count ? m.count : 0}</Info>
-				</ItemG> : null}
-				<Collapse in={Boolean(weather)}>
-					<ItemG container>
-						<ItemG container xs={12}>
-						
-							<ItemG container xs={8}>
-								<ItemG container direction='row' xs={12}>
-									<T>
-										{t("charts.fields.summary")}: {weather ? weather.currently.summary : null}
-									</T>
-								</ItemG>
-								<ItemG container direction='row' xs={12}>
-									<T>
-										{t("charts.fields.temperature")}: {weather ? `${Math.round(weather.currently.temperature)} \u{2103}` : null}
-									</T>
-								</ItemG>
-								<ItemG container direction='row' xs={12}>
-									<T>
-										{t("charts.fields.windspeed")}: {weather ? `${Math.round(weather.currently.windSpeed / 3.6)} m/s` : null}
-									</T>
-								</ItemG>
-								<ItemG container direction='row' xs={12}>
-									<T>
-										{t("charts.fields.humidity")}: {weather ? `${weather.currently.humidity * 100}%` : null}
-									</T>
-								</ItemG>
-								<ItemG container direction='row' xs={12}>
-									<T>
-										{t("charts.fields.pressure")}: {weather ? `${Math.round(weather.currently.pressure)} hPa` : null}
-									</T>
-								</ItemG>
-							</ItemG>
-							{weather ? <ItemG xs={4} container justify={'center'} alignItems={'center'}><WeatherIcon icon={weather.currently.icon} /></ItemG> : null}
-						</ItemG>
-					</ItemG>
-				</Collapse>
-				{noSeeMore ? null : <ItemG xs={12} container justify={'flex-end'}>
-					<Button variant={'text'} style={{ color: teal[500] }} component={Link} to={`/device/${m.id}`}>
-						{t('menus.seeMore')}
-					</Button>
-				</ItemG>}
+	useEffect(() => {
+		const asyncFunc = async () => {
+			await getWeatherFunc()
+		}
+		asyncFunc()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+	// componentDidMount =async () => {
+	// 	await this.getWeather()
+	// }
+
+	const { classes, m, noSeeMore, heatMap, dontShow } = props
+	// const { weather } = this.state
+	return !dontShow ? <Paper className={classes.paper}>
+		<ItemG container>
+			<ItemG xs={10} container direction={'column'}>
+				<Typography variant={'h5'}>{m.name ? m.name : m.id}</Typography>
+				<Typography variant={'body1'}>{`(${m.id})`}</Typography>
 			</ItemG>
-		</Paper> : null
+			<ItemG xs={2} container>
+				<ItemG container justify={'flex-end'}>
+					{renderIcon(m.liveStatus)}
+				</ItemG>
+			</ItemG>
+			<ItemG xs={12}>
+				<Typography variant={'body1'}> {`${t('charts.fields.date')}: ${moment().format('lll')}`}</Typography>
+			</ItemG>
 
-	}
+			<ItemG xs={12}>
+				<Caption>{t('devices.fields.address')}</Caption>
+				<Info>{m.address ? m.address : t('devices.noAddress')}</Info>
+			</ItemG>
+			<ItemG xs={6}>
+				<Caption>{t('devices.fields.temp')}</Caption>
+				<Info>{m.temperature ? `${m.temperature}\u2103` : `-\u2103`}</Info>
+			</ItemG>
+			{heatMap ? <ItemG xs={6}>
+				<Caption>{t('devices.simpleList.totalCount')}</Caption>
+				<Info>{m.count ? m.count : 0}</Info>
+			</ItemG> : null}
+			<Collapse in={Boolean(stateWeather)}>
+				<ItemG container>
+					<ItemG container xs={12}>
+
+						<ItemG container xs={8}>
+							<ItemG container direction='row' xs={12}>
+								<T>
+									{t("charts.fields.summary")}: {stateWeather ? stateWeather.currently.summary : null}
+								</T>
+							</ItemG>
+							<ItemG container direction='row' xs={12}>
+								<T>
+									{t("charts.fields.temperature")}: {stateWeather ? `${Math.round(stateWeather.currently.temperature)} \u{2103}` : null}
+								</T>
+							</ItemG>
+							<ItemG container direction='row' xs={12}>
+								<T>
+									{t("charts.fields.windspeed")}: {stateWeather ? `${Math.round(stateWeather.currently.windSpeed / 3.6)} m/s` : null}
+								</T>
+							</ItemG>
+							<ItemG container direction='row' xs={12}>
+								<T>
+									{t("charts.fields.humidity")}: {stateWeather ? `${stateWeather.currently.humidity * 100}%` : null}
+								</T>
+							</ItemG>
+							<ItemG container direction='row' xs={12}>
+								<T>
+									{t("charts.fields.pressure")}: {stateWeather ? `${Math.round(stateWeather.currently.pressure)} hPa` : null}
+								</T>
+							</ItemG>
+						</ItemG>
+						{stateWeather ? <ItemG xs={4} container justify={'center'} alignItems={'center'}><WeatherIcon icon={stateWeather.currently.icon} /></ItemG> : null}
+					</ItemG>
+				</ItemG>
+			</Collapse>
+			{noSeeMore ? null : <ItemG xs={12} container justify={'flex-end'}>
+				<Button variant={'text'} style={{ color: teal[500] }} component={Link} to={`/device/${m.id}`}>
+					{t('menus.seeMore')}
+				</Button>
+			</ItemG>}
+		</ItemG>
+	</Paper> : null
 }
-const mapStateToProps = (state) => ({
-	lang: state.settings.language,
-	heatData: state.dateTime.heatData,
-	weather: state.weather.weather,
-	loadingWeather: state.weather.loading
-})
 
-
-const mapDispatchToProps = dispatch => ({
-	getWeather: async (device, date, lang) => dispatch(await getWeather(device, date, lang))
-})
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(withLocalization()(withStyles(styles)(OpenPopup)))
+export default withLocalization()(withStyles(styles)(OpenPopup))
