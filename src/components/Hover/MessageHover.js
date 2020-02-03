0c0da5fdd8ba7fc6166f96789d8370ee26cc625c
SromPavel <1074227@ucn.dk>
2020-02-03 12:47:27 +0100

    MessageHover.js => hooks

--- src/components/Hover/MessageHover.js
+++ src/components/Hover/MessageHover.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import { Popper, Paper, withStyles, Fade, /* Divider, Button, IconButton, Tooltip */ } from '@material-ui/core';
 // import T from 'components/Typography/T';
 // import ItemG from 'components/Grid/ItemG';
@@ -6,56 +6,92 @@ import { Popper, Paper, withStyles, Fade, /* Divider, Button, IconButton, Toolti
 import { /* Language,  Star, StarBorder,*/ SignalWifi2Bar, /* LibraryBooks, DataUsage, Business */ } from 'variables/icons';
 import withLocalization from 'components/Localization/T';
 // import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { isFav, removeFromFav, finishedSaving, addToFav } from 'redux/favorites';
 import withSnackbar from 'components/Localization/S';
 import hoverStyles from 'assets/jss/components/hover/hoverStyles'
 
 import { CircularLoader } from 'components';
+import { useSnackbar, useLocalization } from 'hooks';
 
-class MessageHover extends Component {
-	componentDidUpdate = () => {
-		if (this.props.saved === true) {
-			const { message } = this.props
+// const mapStateToProps = (state) => ({
+// 	saved: state.favorites.saved
+// })
+
+// const mapDispatchToProps = dispatch => ({
+// 	isFav: (favObj) => dispatch(isFav(favObj)),
+// 	addToFav: (favObj) => dispatch(addToFav(favObj)),
+// 	removeFromFav: (favObj) => dispatch(removeFromFav(favObj)),
+// 	finishedSaving: () => dispatch(finishedSaving())
+// })
+
+const MessageHover = props => {
+	const t = useLocalization()
+	const s = useSnackbar().s
+	const dispatch = useDispatch()
+	const saved = useSelector(state => state.favorites.saved)
+
+	useEffect(() => {
+		if (saved === true) {
+			const { message } = props
 			if (message) {
 
-				if (this.props.isFav({ id: message.id, type: 'message' })) {
-					this.props.s('snackbars.favorite.saved', { name: message.name, type: this.props.t('favorites.types.message') })
-					this.props.finishedSaving()
+				if (dispatch(isFav({ id: message.id, type: 'message' }))) {
+					s('snackbars.favorite.saved', { name: message.name, type: t('favorites.types.message') })
+					dispatch(finishedSaving())
 				}
-				if (!this.props.isFav({ id: message.id, type: 'message' })) {
-					this.props.s('snackbars.favorite.removed', { name: message.name, type: this.props.t('favorites.types.message') })
-					this.props.finishedSaving()
+				if (!dispatch(isFav({ id: message.id, type: 'message' }))) {
+					s('snackbars.favorite.removed', { name: message.name, type: t('favorites.types.message') })
+					dispatch(finishedSaving())
 				}
 			}
 		}
-	}
-	addToFav = () => {
-		const { message } = this.props
+	}, [dispatch, props, s, saved, t])
+	// componentDidUpdate = () => {
+	// 	if (this.props.saved === true) {
+	// 		const { message } = this.props
+	// 		if (message) {
+
+	// 			if (this.props.isFav({ id: message.id, type: 'message' })) {
+	// 				this.props.s('snackbars.favorite.saved', { name: message.name, type: this.props.t('favorites.types.message') })
+	// 				this.props.finishedSaving()
+	// 			}
+	// 			if (!this.props.isFav({ id: message.id, type: 'message' })) {
+	// 				this.props.s('snackbars.favorite.removed', { name: message.name, type: this.props.t('favorites.types.message') })
+	// 				this.props.finishedSaving()
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// eslint-disable-next-line no-unused-vars
+	const addToFavorites = () => {
+		const { message } = props
 		let favObj = {
 			id: message.id,
 			name: message.name,
 			type: 'message',
 			path: `/message/${message.id}`
 		}
-		this.props.addToFav(favObj)
+		dispatch(addToFav(favObj))
 	}
-	removeFromFav = () => {
-		const { message } = this.props
+	// eslint-disable-next-line no-unused-vars
+	const removeFromFavorites = () => {
+		const { message } = props
 		let favObj = {
 			id: message.id,
 			name: message.name,
 			type: 'message',
 			path: `/message/${message.id}`
 		}
-		this.props.removeFromFav(favObj)
+		dispatch(removeFromFav(favObj))
 
 	}
-	handleClose = () => {
-		this.props.handleClose()
+	const handleClose = () => {
+		props.handleClose()
 	};
-	renderIcon = (status) => {
-		const { classes } = this.props
+	// eslint-disable-next-line no-unused-vars
+	const renderIcon = (status) => {
+		const { classes } = props
 		switch (status) {
 			case 1:
 				return <SignalWifi2Bar className={classes.yellowSignal + ' ' + classes.smallIcon} />
@@ -69,26 +105,26 @@ class MessageHover extends Component {
 				break;
 		}
 	}
-	render() {
-		const { /* t, */ anchorEl, classes, message, /* isFav */ } = this.props
-		return (
-			<Popper
-				style={{ zIndex: 1040 }}
-				disablePortal
-				id="simple-popover"
-				open={Boolean(anchorEl)}
-				anchorEl={anchorEl}
-				onClose={this.handleClose}
-				placement={'top-start'}
-				onMouseLeave={this.handleClose}
-				transition
-			>
-				{({ TransitionProps }) => (
-					<Fade {...TransitionProps} timeout={250}>
-						<Paper className={classes.paper}>
-							{message !== null ?
-								<Fragment>
-									{/* <ItemG container style={{ margin: "8px 0" }}>
+
+	const { /* t, */ anchorEl, classes, message, /* isFav */ } = props
+	return (
+		<Popper
+			style={{ zIndex: 1040 }}
+			disablePortal
+			id="simple-popover"
+			open={Boolean(anchorEl)}
+			anchorEl={anchorEl}
+			onClose={handleClose}
+			placement={'top-start'}
+			onMouseLeave={handleClose}
+			transition
+		>
+			{({ TransitionProps }) => (
+				<Fade {...TransitionProps} timeout={250}>
+					<Paper className={classes.paper}>
+						{message !== null ?
+							<Fragment>
+								{/* <ItemG container style={{ margin: "8px 0" }}>
 										<ItemG xs={3} container justify={'center'} alignItems={'center'}>
 											<DataUsage className={classes.img} />
 										</ItemG>
@@ -145,24 +181,13 @@ class MessageHover extends Component {
 											</Tooltip>
 										</ItemG>
 									</ItemG> */}
-								</Fragment>
-								: <CircularLoader fill />}
-						</Paper>
-					</Fade>
-				)}
-			</Popper>
-		)
-	}
+							</Fragment>
+							: <CircularLoader fill />}
+					</Paper>
+				</Fade>
+			)}
+		</Popper>
+	)
 }
-const mapStateToProps = (state) => ({
-	saved: state.favorites.saved
-})
 
-const mapDispatchToProps = dispatch => ({
-	isFav: (favObj) => dispatch(isFav(favObj)),
-	addToFav: (favObj) => dispatch(addToFav(favObj)),
-	removeFromFav: (favObj) => dispatch(removeFromFav(favObj)),
-	finishedSaving: () => dispatch(finishedSaving())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(withSnackbar()((withLocalization()(withStyles(hoverStyles)(MessageHover)))))
+export default withSnackbar()((withLocalization()(withStyles(hoverStyles)(MessageHover))))
