28a0114fcc7fb0e9f1b209548d5488e413d61f10
SromPavel <1074227@ucn.dk>
2020-02-03 11:52:04 +0100

    CollectionHover.js => hooks

--- src/components/Hover/CollectionHover.js
+++ src/components/Hover/CollectionHover.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { Popper, Paper, withStyles, Fade, Divider, Button, IconButton, Tooltip } from '@material-ui/core';
 import T from 'components/Typography/T';
 import ItemG from 'components/Grid/ItemG';
@@ -6,56 +6,89 @@ import ItemG from 'components/Grid/ItemG';
 import { /* Language, */ Star, StarBorder, SignalWifi2Bar, LibraryBooks, DataUsage, Business } from 'variables/icons';
 import withLocalization from 'components/Localization/T';
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { isFav, removeFromFav, finishedSaving, addToFav } from 'redux/favorites';
 import withSnackbar from 'components/Localization/S';
 import hoverStyles from 'assets/jss/components/hover/hoverStyles'
 
 import { CircularLoader } from 'components';
+import { useSnackbar, useLocalization } from 'hooks';
 
-class CollectionHover extends Component {
-	componentDidUpdate = () => {
-		if (this.props.saved === true) {
-			const { collection } = this.props
+// const mapStateToProps = (state) => ({
+// 	saved: state.favorites.saved
+// })
+
+// const mapDispatchToProps = dispatch => ({
+// 	isFav: (favObj) => dispatch(isFav(favObj)),
+// 	addToFav: (favObj) => dispatch(addToFav(favObj)),
+// 	removeFromFav: (favObj) => dispatch(removeFromFav(favObj)),
+// 	finishedSaving: () => dispatch(finishedSaving())
+// })
+
+const CollectionHover = props => {
+	const s = useSnackbar().s
+	const t = useLocalization()
+	const saved = useSelector(state => state.favorites.saved)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		if (saved === true) {
+			const { collection } = props
 			if (collection) {
 
-				if (this.props.isFav({ id: collection.id, type: 'collection' })) {
-					this.props.s('snackbars.favorite.saved', { name: collection.name, type: this.props.t('favorites.types.collection') })
-					this.props.finishedSaving()
+				if (dispatch(isFav({ id: collection.id, type: 'collection' }))) {
+					s('snackbars.favorite.saved', { name: collection.name, type: t('favorites.types.collection') })
+					dispatch(finishedSaving())
 				}
-				if (!this.props.isFav({ id: collection.id, type: 'collection' })) {
-					this.props.s('snackbars.favorite.removed', { name: collection.name, type: this.props.t('favorites.types.collection') })
-					this.props.finishedSaving()
+				if (!dispatch(isFav({ id: collection.id, type: 'collection' }))) {
+					s('snackbars.favorite.removed', { name: collection.name, type: t('favorites.types.collection') })
+					dispatch(finishedSaving())
 				}
 			}
 		}
-	}
-	addToFav = () => {
-		const { collection } = this.props
+	}, [dispatch, props, s, saved, t])
+	// const componentDidUpdate = () => {
+	// 	if (saved === true) {
+	// 		const { collection } = props
+	// 		if (collection) {
+
+	// 			if (dispatch(isFav({ id: collection.id, type: 'collection' }))) {
+	// 				s('snackbars.favorite.saved', { name: collection.name, type: t('favorites.types.collection') })
+	// 				dispatch(finishedSaving())
+	// 			}
+	// 			if (!this.props.isFav({ id: collection.id, type: 'collection' })) {
+	// 				this.props.s('snackbars.favorite.removed', { name: collection.name, type: this.props.t('favorites.types.collection') })
+	// 				this.props.finishedSaving()
+	// 			}
+	// 		}
+	// 	}
+	// }
+	const addToFavorites = () => {
+		const { collection } = props
 		let favObj = {
 			id: collection.id,
 			name: collection.name,
 			type: 'collection',
 			path: `/collection/${collection.id}`
 		}
-		this.props.addToFav(favObj)
+		dispatch(addToFav(favObj))
 	}
-	removeFromFav = () => {
-		const { collection } = this.props
+	const removeFromFavorites = () => {
+		const { collection } = props
 		let favObj = {
 			id: collection.id,
 			name: collection.name,
 			type: 'collection',
 			path: `/collection/${collection.id}`
 		}
-		this.props.removeFromFav(favObj)
+		dispatch(removeFromFav(favObj))
 
 	}
-	handleClose = () => {
-		this.props.handleClose()
+	const handleClose = () => {
+		props.handleClose()
 	};
-	renderIcon = (status) => {
-		const { classes } = this.props
+	const renderIcon = (status) => {
+		const { classes } = props
 		switch (status) {
 			case 1:
 				return <SignalWifi2Bar className={classes.yellowSignal + ' ' + classes.smallIcon} />
@@ -69,100 +102,89 @@ class CollectionHover extends Component {
 				break;
 		}
 	}
-	render() {
-		const { t, anchorEl, classes, collection, isFav } = this.props
-		return (
-			<Popper
-				style={{ zIndex: 1040 }}
-				disablePortal
-				id="simple-popover"
-				open={Boolean(anchorEl)}
-				anchorEl={anchorEl}
-				onClose={this.handleClose}
-				placement={'top-start'}
-				onMouseLeave={this.handleClose}
-				transition
-			>
-				{({ TransitionProps }) => (
-					<Fade {...TransitionProps} timeout={250}>
-						<Paper className={classes.paper}>
-							{collection !== null ?
-								<Fragment>
-									<ItemG container style={{ margin: "8px 0" }}>
-										<ItemG xs={3} container justify={'center'} alignItems={'center'}>
-											<DataUsage className={classes.img} />
-										</ItemG>
-										<ItemG xs={9} container justify={'center'}>
-											<ItemG xs={12}>
-												<T variant={'h6'} style={{ textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}>
-													{collection.name}
-												</T>
-											</ItemG>
-											<ItemG xs={12}>
-												<T className={classes.smallText} paragraph={false}>{`${collection.id}`}</T>
-											</ItemG>
-										</ItemG>
-									</ItemG>
-									<ItemG xs={12} className={classes.middleContainer}>
-										<ItemG xs={12}>
-											<T className={classes.smallText} paragraph={false}>
-												<Business className={classes.smallIcon} />
-												{`${collection.org.name ? collection.org.name : t('devices.fields.free')}`}
-											</T>
-										</ItemG>
-										<ItemG xs={12}>
-											<T className={classes.smallText}>
-												{this.renderIcon(collection.activeDeviceStats ? collection.activeDeviceStats.state : null)}
-												{collection.activeDeviceStats ?
-													<Link to={{ pathname: `/device/${collection.activeDeviceStats.id}`, prevURL: '/collections' }}>
-														{collection.activeDeviceStats.id}
-													</Link>
-													: t('no.device')}
-											</T>
-										</ItemG>
-										<ItemG xs={12}>
-											<T className={classes.smallText}>
-												<LibraryBooks className={classes.smallIcon} />
-												{collection.project.title ? <Link to={{ pathname: `/project/${collection.project.id}`, prevURL: '/collections' }}>
-													{collection.project.title}
-												</Link> : t('no.project')}
-											</T>
-										</ItemG>
 
+	const { anchorEl, classes, collection } = props
+	return (
+		<Popper
+			style={{ zIndex: 1040 }}
+			disablePortal
+			id="simple-popover"
+			open={Boolean(anchorEl)}
+			anchorEl={anchorEl}
+			onClose={handleClose}
+			placement={'top-start'}
+			onMouseLeave={handleClose}
+			transition
+		>
+			{({ TransitionProps }) => (
+				<Fade {...TransitionProps} timeout={250}>
+					<Paper className={classes.paper}>
+						{collection !== null ?
+							<Fragment>
+								<ItemG container style={{ margin: "8px 0" }}>
+									<ItemG xs={3} container justify={'center'} alignItems={'center'}>
+										<DataUsage className={classes.img} />
 									</ItemG>
-									<Divider />
-									<ItemG container style={{ marginTop: '8px' }}>
-										<ItemG>
-											<Button color={'primary'} variant={'text'} component={Link} to={{ pathname: `/collection/${collection.id}/edit`, prevURL: '/collections' }}>
-												{t('menus.edit')}
-											</Button>
+									<ItemG xs={9} container justify={'center'}>
+										<ItemG xs={12}>
+											<T variant={'h6'} style={{ textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}>
+												{collection.name}
+											</T>
 										</ItemG>
-										<ItemG container style={{ flex: 1, justifyContent: 'flex-end' }}>
-											<Tooltip placement="top" title={isFav({ id: collection.id, type: 'collection' }) ? t('menus.favorites.remove') : t('menus.favorites.add')}>
-												<IconButton className={classes.smallAction} onClick={isFav({ id: collection.id, type: 'collection' }) ? this.removeFromFav : this.addToFav}>
-													{isFav({ id: collection.id, type: 'collection' }) ? <Star /> : <StarBorder />}
-												</IconButton>
-											</Tooltip>
+										<ItemG xs={12}>
+											<T className={classes.smallText} paragraph={false}>{`${collection.id}`}</T>
 										</ItemG>
 									</ItemG>
-								</Fragment>
-								: <CircularLoader fill />}
-						</Paper>
-					</Fade>
-				)}
-			</Popper>
-		)
-	}
+								</ItemG>
+								<ItemG xs={12} className={classes.middleContainer}>
+									<ItemG xs={12}>
+										<T className={classes.smallText} paragraph={false}>
+											<Business className={classes.smallIcon} />
+											{`${collection.org.name ? collection.org.name : t('devices.fields.free')}`}
+										</T>
+									</ItemG>
+									<ItemG xs={12}>
+										<T className={classes.smallText}>
+											{renderIcon(collection.activeDeviceStats ? collection.activeDeviceStats.state : null)}
+											{collection.activeDeviceStats ?
+												<Link to={{ pathname: `/device/${collection.activeDeviceStats.id}`, prevURL: '/collections' }}>
+													{collection.activeDeviceStats.id}
+												</Link>
+												: t('no.device')}
+										</T>
+									</ItemG>
+									<ItemG xs={12}>
+										<T className={classes.smallText}>
+											<LibraryBooks className={classes.smallIcon} />
+											{collection.project.title ? <Link to={{ pathname: `/project/${collection.project.id}`, prevURL: '/collections' }}>
+												{collection.project.title}
+											</Link> : t('no.project')}
+										</T>
+									</ItemG>
+
+								</ItemG>
+								<Divider />
+								<ItemG container style={{ marginTop: '8px' }}>
+									<ItemG>
+										<Button color={'primary'} variant={'text'} component={Link} to={{ pathname: `/collection/${collection.id}/edit`, prevURL: '/collections' }}>
+											{t('menus.edit')}
+										</Button>
+									</ItemG>
+									<ItemG container style={{ flex: 1, justifyContent: 'flex-end' }}>
+										<Tooltip placement="top" title={dispatch(isFav({ id: collection.id, type: 'collection' })) ? t('menus.favorites.remove') : t('menus.favorites.add')}>
+											<IconButton className={classes.smallAction} onClick={dispatch(isFav({ id: collection.id, type: 'collection' })) ? removeFromFavorites : addToFavorites}>
+												{dispatch(isFav({ id: collection.id, type: 'collection' })) ? <Star /> : <StarBorder />}
+											</IconButton>
+										</Tooltip>
+									</ItemG>
+								</ItemG>
+							</Fragment>
+							: <CircularLoader fill />}
+					</Paper>
+				</Fade>
+			)}
+		</Popper>
+	)
 }
-const mapStateToProps = (state) => ({
-	saved: state.favorites.saved
-})
 
-const mapDispatchToProps = dispatch => ({
-	isFav: (favObj) => dispatch(isFav(favObj)),
-	addToFav: (favObj) => dispatch(addToFav(favObj)),
-	removeFromFav: (favObj) => dispatch(removeFromFav(favObj)),
-	finishedSaving: () => dispatch(finishedSaving())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(withSnackbar()((withLocalization()(withStyles(hoverStyles)(CollectionHover)))))
+export default withSnackbar()((withLocalization()(withStyles(hoverStyles)(CollectionHover))))
