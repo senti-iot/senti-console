0fb974e172dff007bc1d0f97654e6417ecd9e4f0
SromPavel <1074227@ucn.dk>
2020-02-04 10:46:50 +0100

    merge

--- src/components/Sensors/SensorCard.js
+++ src/components/Sensors/SensorCard.js
@@ -4,23 +4,6 @@ import { ItemGrid, SmallCard, ItemG, T, Link } from 'components'
 import regularCardStyle from 'assets/jss/material-dashboard-react/regularCardStyle'
 import { MoreVert, Edit, /* PictureAsPdf, Devices, Delete, */ InputIcon, Block, CheckCircle, DeviceHub } from 'variables/icons'
 import { withRouter } from 'react-router-dom'
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-import { useLocalization } from 'hooks'
-
-const SensorCard = props => {
-	const t = useLocalization()
-	const [actionAnchor, setActionAnchor] = useState(null)
-	const [img, /* setImg */] = useState(null)
-	// constructor(props) {
-	//   super(props)
-
-	//   this.state = {
-	// 	 actionAnchor: null,
-	// 	 img: null
-	//   }
-	// }
-=======
 
 class SensorCard extends Component {
 	constructor(props) {
@@ -35,7 +18,6 @@ class SensorCard extends Component {
 	handleOpenActionsDetails = event => {
 		this.setState({ actionAnchor: event.currentTarget });
 	}
->>>>>>> 14bc69624e1552b0a6371abab49cb65e6a20f461
 
 	const handleOpenActionsDetails = event => {
 		setActionAnchor(event.currentTarget)
@@ -69,56 +51,56 @@ class SensorCard extends Component {
 		}
 	}
 	const { p, classes } = props
-	return (
-		<SmallCard
-			avatar={<DeviceHub className={classes.bigIcon} />}
-			key={p.id}
-			title={p.name}
-			subheader={p.uuid}
-			img={img}
-			topAction={
-				<ItemGrid noMargin noPadding>
-					<IconButton
-						aria-label='More'
-						aria-owns={actionAnchor ? 'long-menu' : null}
-						aria-haspopup='true'
-						onClick={handleOpenActionsDetails}>
-						<MoreVert />
-					</IconButton>
-					<Menu
-						id='long-menu'
-						anchorEl={actionAnchor}
-						open={Boolean(actionAnchor)}
-						onClose={handleCloseActionsDetails}
-						PaperProps={{
-							style: {
-								minWidth: 200
-							}
-						}}>
-						<MenuItem onClick={() => props.history.push(`/sensor/${p.id}/edit`)}>
-							<Edit className={classes.leftIcon} />{t('menus.edit')}
-						</MenuItem>
-					</Menu>
-				</ItemGrid>
-			}
-			content={<ItemG container>
-				<ItemG xs={12}>
-					<T className={classes.smallText} paragraph={false}>
-						<InputIcon className={classes.icon} />
-						<Link to={{ pathname: `/sensor/${p.reg_id}` }}>{p.reg_name}</Link>
-					</T>
-				</ItemG>
-				<ItemG xs={12}>
-					{renderCommunication(p.communication)}
-				</ItemG>
-			</ItemG>}
-			rightActions={
-				<Button variant={'text'} color={'primary'} onClick={() => props.history.push(`/sensor/${p.id}`)}>
-					{t('menus.seeMore')}
-				</Button>
-			}
-		/>
-	)
+return (
+	<SmallCard
+		avatar={<DeviceHub className={classes.bigIcon} />}
+		key={p.id}
+		title={p.name}
+		subheader={p.uuid}
+		img={img}
+		topAction={
+			<ItemGrid noMargin noPadding>
+				<IconButton
+					aria-label='More'
+					aria-owns={actionAnchor ? 'long-menu' : null}
+					aria-haspopup='true'
+					onClick={handleOpenActionsDetails}>
+					<MoreVert />
+				</IconButton>
+				<Menu
+					id='long-menu'
+					anchorEl={actionAnchor}
+					open={Boolean(actionAnchor)}
+					onClose={handleCloseActionsDetails}
+					PaperProps={{
+						style: {
+							minWidth: 200
+						}
+					}}>
+					<MenuItem onClick={() => props.history.push(`/sensor/${p.id}/edit`)}>
+						<Edit className={classes.leftIcon} />{t('menus.edit')}
+					</MenuItem>
+				</Menu>
+			</ItemGrid>
+		}
+		content={<ItemG container>
+			<ItemG xs={12}>
+				<T className={classes.smallText} paragraph={false}>
+					<InputIcon className={classes.icon} />
+					<Link to={{ pathname: `/sensor/${p.reg_id}` }}>{p.reg_name}</Link>
+				</T>
+			</ItemG>
+			<ItemG xs={12}>
+				{renderCommunication(p.communication)}
+			</ItemG>
+		</ItemG>}
+		rightActions={
+			<Button variant={'text'} color={'primary'} onClick={() => props.history.push(`/sensor/${p.id}`)}>
+				{t('menus.seeMore')}
+			</Button>
+		}
+	/>
+)
 }
 
 export default withRouter(withStyles(regularCardStyle)(SensorCard))
