e59fc315cf10aea67169a3ed15f9d0799f5f8658
SromPavel <1074227@ucn.dk>
2020-02-04 11:34:57 +0100

    CreateSensorForm.js => hooks

--- src/components/Sensors/CreateSensorForm.js
+++ src/components/Sensors/CreateSensorForm.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { useState, Fragment } from 'react'
 import { Dialog, AppBar, Toolbar, Typography, Button, List, ListItem, ListItemText, Divider, withStyles, Hidden, IconButton, InputAdornment, Tooltip } from '@material-ui/core';
 import { Close, CheckCircle, Block } from 'variables/icons';
 import cx from 'classnames'
@@ -12,30 +12,37 @@ import Info from 'components/Typography/Info';
 import AssignDeviceTypeDialog from 'components/AssignComponents/AssignDeviceTypeDialog';
 import AssignRegistryDialog from 'components/AssignComponents/AssignRegistryDialog';
 import AssignCFDialog from 'components/AssignComponents/AssignCFDialog';
+import { useLocalization } from 'hooks';
 
 /**
 * @augments {Component<{	t:Function.isRequired,	collection:object.isRequired,	handleChangeDevice:Function.isRequired,	handleCloseDevice:Function.isRequired,	handleOpenDevice:Function.isRequired,	open:boolean.isRequired,	devices:array.isRequired,	device:object.isRequired,	handleCreate:Function.isRequired,	handleChange:Function.isRequired,>}
 */
-class CreateSensorForm extends Component {
-	constructor(props) {
-		super(props)
 
-		this.state = {
-			filters: {
-				keyword: ''
-			}
-		}
-	}
+// @Andrei
+const CreateSensorForm = props => {
+	const t = useLocalization()
+	const [filters, setFilters] = useState({ keyword: '' })
+	// constructor(props) {
+	// 	super(props)
 
-	handleFilterKeyword = value => {
-		this.setState({
-			filters: {
-				keyword: value
-			}
-		})
+	// 	this.state = {
+	// 		filters: {
+	// 			keyword: ''
+	// 		}
+	// 	}
+	// }
+
+	const handleFilterKeyword = value => {
+		setFilters({ ...filters, keyword: value })
+		// this.setState({
+		// 	filters: {
+		// 		keyword: value
+		// 	}
+		// })
 	}
-	ProtocolTypes = () => {
-		const { t } = this.props
+	// eslint-disable-next-line no-unused-vars
+	const ProtocolTypes = () => {
+		// const { t } = this.props
 		return [
 			{ value: 0, label: t('sensors.fields.protocols.none') },
 			{ value: 1, label: t('sensors.fields.protocols.mqtt') },
@@ -44,15 +51,15 @@ class CreateSensorForm extends Component {
 		]
 	}
 
-	CommunicationTypes = () => {
-		const { t, classes } = this.props
+	const CommunicationTypes = () => {
+		const { classes } = props
 		return [
 			{ value: 0, icon: <Block className={classes.blocked} />, label: t('sensors.fields.communications.blocked') },
 			{ value: 1, icon: <CheckCircle className={classes.allowed} />, label: t('sensors.fields.communications.allowed') }
 		]
 	}
-	LocationTypes = () => {
-		const { t } = this.props
+	const LocationTypes = () => {
+		// const { t } = this.props
 		return [
 			{ id: 1, label: t('devices.locationTypes.pedStreet') },
 			{ id: 2, label: t('devices.locationTypes.park') },
@@ -66,9 +73,10 @@ class CreateSensorForm extends Component {
 			{ id: 0, label: t('devices.locationTypes.unspecified') }]
 	}
 
-	renderSelectFunction = () => {
-		const { t, openCF, handleCloseFunc, cfunctions, handleChangeFunc, classes } = this.props
-		const { filters } = this.state
+	// eslint-disable-next-line no-unused-vars
+	const renderSelectFunction = () => {
+		const { openCF, handleCloseFunc, cfunctions, handleChangeFunc, classes } = props
+		// const { filters } = this.state
 		const appBarClasses = cx({
 			[' ' + classes['primary']]: 'primary'
 		});
@@ -95,7 +103,7 @@ class CreateSensorForm extends Component {
 									open={true}
 									focusOnMount
 									suggestions={cfunctions ? suggestionGen(cfunctions) : []}
-									handleFilterKeyword={this.handleFilterKeyword}
+									handleFilterKeyword={handleFilterKeyword}
 									searchValue={filters.keyword} />
 							</ItemG>
 						</ItemG>
@@ -117,7 +125,7 @@ class CreateSensorForm extends Component {
 									open={true}
 									focusOnMount
 									suggestions={cfunctions ? suggestionGen(cfunctions) : []}
-									handleFilterKeyword={this.handleFilterKeyword}
+									handleFilterKeyword={handleFilterKeyword}
 									searchValue={filters.keyword} />
 							</ItemG>
 						</ItemG>
@@ -137,8 +145,8 @@ class CreateSensorForm extends Component {
 		</Dialog>
 	}
 
-	renderMetadata = () => {
-		const { sensorMetadata, handleRemoveMtdKey, handleAddMetadataKey, t, handleChangeMetadata, handleChangeMetadataKey, handleChangeKey, handleOpenFunc, handleChangeType, cfunctions, classes, handleRemoveKey, handleRemoveFunction, handleAddKey } = this.props
+	const renderMetadata = () => {
+		const { sensorMetadata, handleRemoveMtdKey, handleAddMetadataKey, handleChangeMetadata, handleChangeMetadataKey, handleChangeKey, handleOpenFunc, handleChangeType, cfunctions, classes, handleRemoveKey, handleRemoveFunction, handleAddKey } = props
 		return <Fragment>
 			<T variant={'subtitle1'}>{t('sensors.fields.metadata')}</T>
 			{sensorMetadata.metadata.length > 0 ? sensorMetadata.metadata.map((m, i) => {
@@ -232,8 +240,8 @@ class CreateSensorForm extends Component {
 		</Fragment>
 	}
 
-	renderMetadataInbound = () => {
-		const { sensorMetadata, cfunctions, t, handleAddInboundFunction, handleOpenFunc, handleRemoveInboundFunction, classes } = this.props
+	const renderMetadataInbound = () => {
+		const { sensorMetadata, cfunctions, handleAddInboundFunction, handleOpenFunc, handleRemoveInboundFunction, classes } = props
 		return <Fragment>
 			{sensorMetadata.inbound.map((p, i) => {
 				return <ItemGrid xs={12} container alignItems={'center'}>
@@ -265,174 +273,173 @@ class CreateSensorForm extends Component {
 
 	}
 
-	render() {
-		const { t,
-			handleOpenReg, openReg, handleCloseReg, handleChangeReg,
-			handleOpenDT, handleCloseDT, openDT, handleChangeDT,
-			openCF, handleCloseFunc, handleChangeFunc,
-			handleChange, sensor, getLatLngFromMap,
-			classes, handleCreate, goToRegistries, select } = this.props
-		return (
-			<GridContainer>
-				<ItemGrid xs={12}>
-					<InfoCard
-						title={t('devices.fields.description')}
-						noExpand
-						noHeader
-						content={
-							<ItemG>
 
-								<AssignCFDialog
+	const {
+		handleOpenReg, openReg, handleCloseReg, handleChangeReg,
+		handleOpenDT, handleCloseDT, openDT, handleChangeDT,
+		openCF, handleCloseFunc, handleChangeFunc,
+		handleChange, sensor, getLatLngFromMap,
+		classes, handleCreate, goToRegistries, select } = props
+	return (
+		<GridContainer>
+			<ItemGrid xs={12}>
+				<InfoCard
+					title={t('devices.fields.description')}
+					noExpand
+					noHeader
+					content={
+						<ItemG>
+
+							<AssignCFDialog
+								t={t}
+								open={openCF.open}
+								handleClose={handleCloseFunc}
+								callBack={cf => {
+									handleChangeFunc(cf, openCF.where)
+									handleCloseFunc()
+								}}
+							/>
+							{/* {this.renderSelectFunction()} */}
+							<ItemGrid xs={12}>
+								<TextF
+									id={'sensorName'}
+									label={t('devices.fields.name')}
+									onChange={handleChange('name')}
+									value={sensor.name}
+									autoFocus
+								/>
+
+							</ItemGrid>
+							<ItemGrid xs={12}>
+								<TextF
+									id={'sensorDescription'}
+									label={t('devices.fields.description')}
+									onChange={handleChange('description')}
+									value={sensor.description}
+									multiline
+									rows={3}
+								/>
+							</ItemGrid>
+							<ItemGrid xs={12}>
+								{/* {this.renderSelectReg()} */}
+								<AssignRegistryDialog
 									t={t}
-									open={openCF.open}
-									handleClose={handleCloseFunc}
-									callBack={cf => {
-										handleChangeFunc(cf, openCF.where)
-										handleCloseFunc()
+									open={openReg}
+									handleClose={handleCloseReg}
+									callBack={registry => {
+										handleChangeReg(registry)
+										handleCloseReg()
 									}}
 								/>
-								{/* {this.renderSelectFunction()} */}
-								<ItemGrid xs={12}>
-									<TextF
-										id={'sensorName'}
-										label={t('devices.fields.name')}
-										onChange={handleChange('name')}
-										value={sensor.name}
-										autoFocus
+								<TextF
+									id={'regID'}
+									label={t('sensors.fields.registry')}
+									value={select.reg.name}
+									readOnly
+									onClick={handleOpenReg}
+									onChange={() => { }}
+									InputProps={{
+										onChange: handleOpenReg,
+										readOnly: true
+									}}
+								/>
+							</ItemGrid>
+							<Divider style={{ margin: "16px" }} />
+							<ItemGrid xs={12}>
+								<AssignDeviceTypeDialog
+									t={t}
+									open={openDT}
+									handleClose={handleCloseDT}
+									callBack={deviceType => {
+										handleChangeDT(deviceType)
+										handleCloseDT()
+									}}
+								/>
+								<TextF
+									id={'regID'}
+									label={t('sensors.fields.deviceType')}
+									value={select.dt.name ? select.dt.name : ''}
+									readOnly
+									onClick={handleOpenDT}
+									onChange={() => { }}
+									InputProps={{
+										onChange: handleOpenDT,
+										readOnly: true
+									}}
+								/>
+							</ItemGrid>
+							{renderMetadata()}
+							<Divider style={{ margin: "16px" }} />
+							{renderMetadataInbound()}
+							<Divider style={{ margin: "16px" }} />
+							<ItemGrid xs={12}>
+								{/* <ItemG xs={12}> */}
+								<div style={{ height: 400 }}>
+									<OpenStreetMap
+										mapTheme={props.mapTheme}
+										calibrate
+										height={400}
+										width={400}
+										markers={[{ lat: sensor.lat ? sensor.lat : 56, long: sensor.lng ? sensor.lng : 9 }]}
+										getLatLng={getLatLngFromMap}
 									/>
+								</div>
+								{/* </ItemG> */}
+							</ItemGrid>
+							<ItemGrid xs={12}>
+								<Info>
+									{`${sensor.lat} ${sensor.lng}`}
+								</Info>
+								<TextF
+									id={'sensorName'}
+									label={t('devices.fields.address')}
+									onChange={handleChange('address')}
+									value={sensor.address}
+								/>
+							</ItemGrid>
+							<Divider style={{ margin: "16px" }} />
+							<ItemGrid xs={12}>
+								<DSelect
+									label={t('devices.fields.locType')}
+									value={sensor.locType}
+									onChange={handleChange('locType')}
+									menuItems={LocationTypes().map(m => ({
+										value: m.id,
+										label: m.label
+									}))}
+								/>
+							</ItemGrid>
+							<ItemGrid xs={12}>
+								<DSelect
+									label={t('sensors.fields.communication')}
+									onChange={handleChange('communication')}
+									value={sensor.communication}
+									menuItems={CommunicationTypes()}
+								/>
+							</ItemGrid>
+							<ItemGrid container>
+								<div className={classes.wrapper}>
+									<Button
+										variant='outlined'
+										onClick={goToRegistries}
+										className={classes.redButton}
+									>
+										{t('actions.cancel')}
+									</Button>
+								</div>
+								<div className={classes.wrapper}>
+									<Button onClick={handleCreate} variant={'outlined'} color={'primary'}>
+										{t('actions.save')}
+									</Button>
+								</div>
+							</ItemGrid>
+						</ItemG>
+					}
+				/>
+			</ItemGrid>
+		</GridContainer>
 
-								</ItemGrid>
-								<ItemGrid xs={12}>
-									<TextF
-										id={'sensorDescription'}
-										label={t('devices.fields.description')}
-										onChange={handleChange('description')}
-										value={sensor.description}
-										multiline
-										rows={3}
-									/>
-								</ItemGrid>
-								<ItemGrid xs={12}>
-									{/* {this.renderSelectReg()} */}
-									<AssignRegistryDialog
-										t={t}
-										open={openReg}
-										handleClose={handleCloseReg}
-										callBack={registry => {
-											handleChangeReg(registry)
-											handleCloseReg()
-										}}
-									/>
-									<TextF
-										id={'regID'}
-										label={t('sensors.fields.registry')}
-										value={select.reg.name}
-										readOnly
-										onClick={handleOpenReg}
-										onChange={() => { }}
-										InputProps={{
-											onChange: handleOpenReg,
-											readOnly: true
-										}}
-									/>
-								</ItemGrid>
-								<Divider style={{ margin: "16px" }} />
-								<ItemGrid xs={12}>
-									<AssignDeviceTypeDialog
-										t={t}
-										open={openDT}
-										handleClose={handleCloseDT}
-										callBack={deviceType => {
-											handleChangeDT(deviceType)
-											handleCloseDT()
-										}}
-									/>
-									<TextF
-										id={'regID'}
-										label={t('sensors.fields.deviceType')}
-										value={select.dt.name ? select.dt.name : ''}
-										readOnly
-										onClick={handleOpenDT}
-										onChange={() => { }}
-										InputProps={{
-											onChange: handleOpenDT,
-											readOnly: true
-										}}
-									/>
-								</ItemGrid>
-								{this.renderMetadata()}
-								<Divider style={{ margin: "16px" }} />
-								{this.renderMetadataInbound()}
-								<Divider style={{ margin: "16px" }} />
-								<ItemGrid xs={12}>
-									{/* <ItemG xs={12}> */}
-									<div style={{ height: 400 }}>
-										<OpenStreetMap
-											mapTheme={this.props.mapTheme}
-											calibrate
-											height={400}
-											width={400}
-											markers={[{ lat: sensor.lat ? sensor.lat : 56, long: sensor.lng ? sensor.lng : 9 }]}
-											getLatLng={getLatLngFromMap}
-										/>
-									</div>
-									{/* </ItemG> */}
-								</ItemGrid>
-								<ItemGrid xs={12}>
-									<Info>
-										{`${sensor.lat} ${sensor.lng}`}
-									</Info>
-									<TextF
-										id={'sensorName'}
-										label={t('devices.fields.address')}
-										onChange={handleChange('address')}
-										value={sensor.address}
-									/>
-								</ItemGrid>
-								<Divider style={{ margin: "16px" }} />
-								<ItemGrid xs={12}>
-									<DSelect
-										label={t('devices.fields.locType')}
-										value={sensor.locType}
-										onChange={handleChange('locType')}
-										menuItems={this.LocationTypes().map(m => ({
-											value: m.id,
-											label: m.label
-										}))}
-									/>
-								</ItemGrid>
-								<ItemGrid xs={12}>
-									<DSelect
-										label={t('sensors.fields.communication')}
-										onChange={handleChange('communication')}
-										value={sensor.communication}
-										menuItems={this.CommunicationTypes()}
-									/>
-								</ItemGrid>
-								<ItemGrid container>
-									<div className={classes.wrapper}>
-										<Button
-											variant='outlined'
-											onClick={goToRegistries}
-											className={classes.redButton}
-										>
-											{t('actions.cancel')}
-										</Button>
-									</div>
-									<div className={classes.wrapper}>
-										<Button onClick={handleCreate} variant={'outlined'} color={'primary'}>
-											{t('actions.save')}
-										</Button>
-									</div>
-								</ItemGrid>
-							</ItemG>
-						}
-					/>
-				</ItemGrid>
-			</GridContainer>
-
-		)
-	}
+	)
 }
 
 
