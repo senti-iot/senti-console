d014ac3753dc22ec4a05830275c1bc34ceb5c242
SromPavel <1074227@ucn.dk>
2020-02-05 11:51:50 +0100

    CollectionsCards.js => hooks

--- src/views/Collections/CollectionsCards.js
+++ src/views/Collections/CollectionsCards.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import CollectionCard from 'components/Collections/CollectionCard';
 import { ItemG, GridContainer } from 'components';
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import devicetableStyles from 'assets/jss/components/devices/devicetableStyles';
 import { withStyles } from '@material-ui/core';
 import CP from 'components/Table/CP';
+import { useLocalization } from 'hooks'
 
-class CollectionsCards extends Component {
-	constructor(props) {
-	  super(props)
-	
-		this.state = {
-			page: 0
-	  }
-	}
-	
-	static propTypes = {
-		collections: PropTypes.array,
-	}
-	
-	handleChangePage = (event, page) => {
-		this.setState({ page });
+// const mapStateToProps = (state) => ({
+// 	rowsPerPage: state.appState.CPP
+// })
+
+// @Andrei
+const CollectionsCards = props => {
+	const t = useLocalization()
+	const rowsPerPage = useSelector(state => state.appState.CPP)
+	const [page, setPage] = useState(0)
+	// constructor(props) {
+	//   super(props)
+
+	// 	this.state = {
+	// 		page: 0
+	//   }
+	// }
+
+	const handleChangePage = (event, newpage) => {
+		setPage(newpage)
+		// this.setState({ page });
 	}
 
-	render() {
-		const { collections, t,  rowsPerPage, classes } = this.props
-		const { page } = this.state
-		return (
-			<GridContainer spacing={2} justify={'center'}>			
-				{collections ? collections.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((p, i) =>
-					<ItemG container key={i} justify={'center'} xs={12} sm={6} md={4}><CollectionCard history={this.props.history} t={t} key={p.id} d={p} /></ItemG>
-				) : null}
-				<ItemG xs={12}>
-					<CP
-						count={collections ? collections.length : 0}
-						classes={classes}
-						page={page}
-						t={t}
-						handleChangePage={this.handleChangePage}
-					/>
-				</ItemG>
-			</GridContainer>
+	const { collections, classes } = props
+	// const { page } = this.state
+	return (
+		<GridContainer spacing={2} justify={'center'}>
+			{collections ? collections.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((p, i) =>
+				<ItemG container key={i} justify={'center'} xs={12} sm={6} md={4}><CollectionCard history={props.history} t={t} key={p.id} d={p} /></ItemG>
+			) : null}
+			<ItemG xs={12}>
+				<CP
+					count={collections ? collections.length : 0}
+					classes={classes}
+					page={page}
+					t={t}
+					handleChangePage={handleChangePage}
+				/>
+			</ItemG>
+		</GridContainer>
 
-		)
-	}
-}
-const mapStateToProps = (state) => ({
-	rowsPerPage: state.appState.CPP
-})
-
-const mapDispatchToProps = {
+	)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(devicetableStyles, { withTheme: true })(CollectionsCards))
+CollectionsCards.propTypes = {
+	collections: PropTypes.array,
+}
+
+export default withStyles(devicetableStyles, { withTheme: true })(CollectionsCards)
