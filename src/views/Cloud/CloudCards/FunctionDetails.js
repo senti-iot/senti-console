61b340d9e0d4cf5cf2d8b674bfef91629020cbeb
SromPavel <1074227@ucn.dk>
2020-02-05 14:11:43 +0100

    FunctionDetails.js => hooks

--- src/views/Cloud/CloudCards/FunctionDetails.js
+++ src/views/Cloud/CloudCards/FunctionDetails.js
@@ -3,15 +3,19 @@ import cloudfunctionStyles from 'assets/jss/views/deviceStyles';
 import { Caption, ItemG, Info } from 'components';
 import InfoCard from 'components/Cards/InfoCard';
 import Dropdown from 'components/Dropdown/Dropdown';
-import React, { Component } from 'react';
+import React from 'react';
 // import { Link } from 'react-router-dom';
 import { DataUsage, Edit, Delete, /* DeviceHub, LibraryBooks, LayersClear, */ Star, StarBorder, /* Delete */ } from 'variables/icons';
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { useLocalization } from 'hooks'
 
-class RegistryDetails extends Component {
+// @Andrei
+const RegistryDetails = props => {
+	const t = useLocalization()
+	const detailsPanel = useSelector(state => state.settings.detailsPanel)
 
-	renderProtocol = (id) => {
-		const { t } = this.props
+	const renderProtocol = (id) => {
+		// const { t } = this.props
 		switch (id) {
 			case 0:
 				return t('cloudfunctions.fields.types.function')
@@ -21,45 +25,40 @@ class RegistryDetails extends Component {
 				break;
 		}
 	}
-	render() {
-		const { handleOpenDeleteDialog, classes, cloudfunction, t, isFav, addToFav, removeFromFav, detailsPanel, /* accessLevel ,*/ history } = this.props
-		return (
-			<InfoCard
-				title={cloudfunction.name ? cloudfunction.name : cloudfunction.uuid}
-				avatar={<DataUsage />}
-				noExpand
-				expanded={Boolean(detailsPanel)}
-				topAction={<Dropdown menuItems={
-					[
-						{ label: t('menus.edit'), icon: <Edit className={classes.leftIcon} />, func: () => history.push({ pathname: `/function/${cloudfunction.id}/edit`, prevURL: `/function/${cloudfunction.id}` }) },
-						{ label: isFav ? t('menus.favorites.remove') : t('menus.favorites.add'), icon: isFav ? <Star className={classes.leftIcon} /> : <StarBorder className={classes.leftIcon} />, func: isFav ? removeFromFav : addToFav },
-						{ label: t('menus.delete'), icon: <Delete className={classes.leftIcon} />, func: handleOpenDeleteDialog },
 
-					]
-				} />
+	const { handleOpenDeleteDialog, classes, cloudfunction, isFav, addToFav, removeFromFav, /* accessLevel ,*/ history } = props
+	return (
+		<InfoCard
+			title={cloudfunction.name ? cloudfunction.name : cloudfunction.uuid}
+			avatar={<DataUsage />}
+			noExpand
+			expanded={Boolean(detailsPanel)}
+			topAction={<Dropdown menuItems={
+				[
+					{ label: t('menus.edit'), icon: <Edit className={classes.leftIcon} />, func: () => history.push({ pathname: `/function/${cloudfunction.id}/edit`, prevURL: `/function/${cloudfunction.id}` }) },
+					{ label: isFav ? t('menus.favorites.remove') : t('menus.favorites.add'), icon: isFav ? <Star className={classes.leftIcon} /> : <StarBorder className={classes.leftIcon} />, func: isFav ? removeFromFav : addToFav },
+					{ label: t('menus.delete'), icon: <Delete className={classes.leftIcon} />, func: handleOpenDeleteDialog },
 
-				}
-				subheader={<ItemG container alignItems={'center'}>
-					<Caption>{t('registries.fields.id')}:</Caption>&nbsp;{cloudfunction.id}
-				</ItemG>}
-				content={
-					<ItemG container>
-						<ItemG xs={12} md>
-							<Caption>{t('cloudfunctions.fields.type')}</Caption>
-							<Info>{this.renderProtocol(cloudfunction.type)}</Info>
-						</ItemG>
-						<ItemG xs={12}>
-							<Caption>{t('registries.fields.description')}</Caption>
-							<Info>{cloudfunction.description}</Info>
-						</ItemG>
+				]
+			} />
+
+			}
+			subheader={<ItemG container alignItems={'center'}>
+				<Caption>{t('registries.fields.id')}:</Caption>&nbsp;{cloudfunction.id}
+			</ItemG>}
+			content={
+				<ItemG container>
+					<ItemG xs={12} md>
+						<Caption>{t('cloudfunctions.fields.type')}</Caption>
+						<Info>{renderProtocol(cloudfunction.type)}</Info>
 					</ItemG>
-				} />
-		)
-	}
+					<ItemG xs={12}>
+						<Caption>{t('registries.fields.description')}</Caption>
+						<Info>{cloudfunction.description}</Info>
+					</ItemG>
+				</ItemG>
+			} />
+	)
 }
 
-const mapStateToProps = (state) => ({
-	detailsPanel: state.settings.detailsPanel
-})
-
-export default connect(mapStateToProps)(withStyles(cloudfunctionStyles)(RegistryDetails))
+export default withStyles(cloudfunctionStyles)(RegistryDetails)
