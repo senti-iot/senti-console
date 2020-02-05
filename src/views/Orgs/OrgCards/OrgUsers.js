a341ace1a7acbfe460f25c9bf4197a772b8a353c
SromPavel <1074227@ucn.dk>
2020-02-05 13:11:05 +0100

    OrgUsers.js => hooks

--- src/views/Orgs/OrgCards/OrgUsers.js
+++ src/views/Orgs/OrgCards/OrgUsers.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { InfoCard, ItemGrid, Info, Caption, ItemG } from 'components'
 import { Table, TableBody, TableRow, Hidden, withStyles, Link } from '@material-ui/core'
 import { People } from 'variables/icons'
@@ -7,68 +7,69 @@ import devicetableStyles from 'assets/jss/components/devices/devicetableStyles'
 import Gravatar from 'react-gravatar'
 import { pF, dateFormat } from 'variables/functions'
 import moment from 'moment'
+import { useLocalization } from 'hooks'
 
-class OrgUsers extends Component {
-	render() {
-		const { users, classes, t } = this.props
-		return (
-			<InfoCard
-				title={t('users.pageTitle')}
-				avatar={<People />}
-				noExpand
-				noPadding
-				content={
-					<Table>
-						<TableBody style={{ padding: "0 24px" }}>
-							{users ? users.map((n, i) => {
-								const lastLoggedIn = moment(n.lastLoggedIn).isValid() ? dateFormat(n.lastLoggedIn) : t('users.fields.neverLoggedIn')
-								return (
-									<TableRow
-										hover
-										onClick={e => { e.stopPropagation(); this.props.history.push({ pathname: '/management/user/' + n.id, prevURL: `/management/org/${this.props.org.id}` }) }}
-										key={i}
-										style={{ cursor: 'pointer', padding: '0 20px' }}
-									>
-										<Hidden lgUp>
-											<TC className={classes.orgUsersTD} checkbox content={<ItemGrid container zeroMargin justify={'center'}>
-												{n.img ? <img src={n.img} alt='brken' className={classes.img} /> : <Gravatar default='mp' email={n.email} className={classes.img} />}
-											</ItemGrid>} />
-											<TC content={
-												<ItemGrid container zeroMargin noPadding alignItems={'center'}>
-													<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
-														<Info noWrap paragraphCell={classes.noMargin}>
-															{`${n.firstName} ${n.lastName}`}
-														</Info>
-													</ItemGrid>
-													<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
-														<Caption noWrap className={classes.noMargin}>
-															{`${n.org ? n.org.name : t('users.fields.noOrg')} - ${n.email}`}
-														</Caption>
-													</ItemGrid>
-													{/* </ItemGrid> */}
+const OrgUsers = props => {
+	const t = useLocalization()
+
+	const { users, classes } = props
+	return (
+		<InfoCard
+			title={t('users.pageTitle')}
+			avatar={<People />}
+			noExpand
+			noPadding
+			content={
+				<Table>
+					<TableBody style={{ padding: "0 24px" }}>
+						{users ? users.map((n, i) => {
+							const lastLoggedIn = moment(n.lastLoggedIn).isValid() ? dateFormat(n.lastLoggedIn) : t('users.fields.neverLoggedIn')
+							return (
+								<TableRow
+									hover
+									onClick={e => { e.stopPropagation(); props.history.push({ pathname: '/management/user/' + n.id, prevURL: `/management/org/${props.org.id}` }) }}
+									key={i}
+									style={{ cursor: 'pointer', padding: '0 20px' }}
+								>
+									<Hidden lgUp>
+										<TC className={classes.orgUsersTD} checkbox content={<ItemGrid container zeroMargin justify={'center'}>
+											{n.img ? <img src={n.img} alt='brken' className={classes.img} /> : <Gravatar default='mp' email={n.email} className={classes.img} />}
+										</ItemGrid>} />
+										<TC content={
+											<ItemGrid container zeroMargin noPadding alignItems={'center'}>
+												<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
+													<Info noWrap paragraphCell={classes.noMargin}>
+														{`${n.firstName} ${n.lastName}`}
+													</Info>
 												</ItemGrid>
-											} />
+												<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
+													<Caption noWrap className={classes.noMargin}>
+														{`${n.org ? n.org.name : t('users.fields.noOrg')} - ${n.email}`}
+													</Caption>
+												</ItemGrid>
+												{/* </ItemGrid> */}
+											</ItemGrid>
+										} />
 
-										</Hidden>
-										<Hidden mdDown>
-											<TC className={classes.orgUsersTD} checkbox content={<ItemG container justify={'center'}>
-												{n.img ? <img src={n.img} alt='brken' className={classes.img} /> : <Gravatar default='mp' email={n.email} className={classes.img} />}
-											</ItemG>} />
-											{/* <TC label={n.userName} /> */}
-											<TC FirstC label={`${n.firstName} ${n.lastName}`} />
-											<TC label={<Link onClick={e => e.stopPropagation()} href={`tel:${n.phone}`}>{n.phone ? pF(n.phone) : n.phone}</Link>} />
-											<TC label={<Link onClick={e => e.stopPropagation()} href={`mailto:${n.email}`}>{n.email}</Link>} />
-											<TC label={lastLoggedIn} />
-										</Hidden>
-									</TableRow>
+									</Hidden>
+									<Hidden mdDown>
+										<TC className={classes.orgUsersTD} checkbox content={<ItemG container justify={'center'}>
+											{n.img ? <img src={n.img} alt='brken' className={classes.img} /> : <Gravatar default='mp' email={n.email} className={classes.img} />}
+										</ItemG>} />
+										{/* <TC label={n.userName} /> */}
+										<TC FirstC label={`${n.firstName} ${n.lastName}`} />
+										<TC label={<Link onClick={e => e.stopPropagation()} href={`tel:${n.phone}`}>{n.phone ? pF(n.phone) : n.phone}</Link>} />
+										<TC label={<Link onClick={e => e.stopPropagation()} href={`mailto:${n.email}`}>{n.email}</Link>} />
+										<TC label={lastLoggedIn} />
+									</Hidden>
+								</TableRow>
 
-								)
-							}) : null}
-						</TableBody>
-					</Table>
-				} />
-		)
-	}
+							)
+						}) : null}
+					</TableBody>
+				</Table>
+			} />
+	)
 }
 
 export default withStyles(devicetableStyles)(OrgUsers)
