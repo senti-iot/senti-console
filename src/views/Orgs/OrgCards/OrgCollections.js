f861f8b5e1cf4551bf94d46ed85307e165a27319
SromPavel <1074227@ucn.dk>
2020-02-05 12:18:53 +0100

    OrgCollections.js => hooks

--- src/views/Orgs/OrgCards/OrgCollections.js
+++ src/views/Orgs/OrgCards/OrgCollections.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { InfoCard, ItemGrid, Info, Caption, ItemG } from 'components'
 import { Table, TableBody, TableRow, Hidden, withStyles } from '@material-ui/core'
 import { SignalWifi2Bar, DataUsage } from 'variables/icons'
 import TC from 'components/Table/TC'
 import devicetableStyles from 'assets/jss/components/devices/devicetableStyles'
+import { useLocalization } from 'hooks'
 
-
-class OrgCollections extends Component {
-	renderIcon = (status) => {
-		const { classes, t } = this.props
+const OrgCollections = props => {
+	const t = useLocalization()
+	const renderIcon = (status) => {
+		const { classes } = props
 		switch (status) {
 			case 1:
 				return <ItemG container justify={'center'} title={t('devices.status.yellow')}>
@@ -30,61 +31,60 @@ class OrgCollections extends Component {
 				break;
 		}
 	}
-	render() {
-		const { collections, classes, t } = this.props
-		return (
-			<InfoCard
-				title={t('collections.pageTitle')}
-				subheader={<ItemG><Caption>
-					{`${t('orgs.fields.collectionCount')}: ${collections.length}`}
-				</Caption></ItemG>}
-				avatar={<DataUsage />}
-				noExpand
-				noPadding
-				content={
-					<Table>
-						<TableBody style={{ padding: "0 24px" }}>
-							{collections ? collections.map((n, i) => {
-								return (
-									<TableRow
-										hover
-										onClick={e => { e.stopPropagation(); this.props.history.push({ pathname: '/collection/' + n.id, prevURL: `/management/org/${this.props.org.id}` }) }}
-										key={i}
-										style={{ cursor: 'pointer', padding: '0 20px' }}
-									>
-										<Hidden lgUp>
-											<TC checkbox className={classes.orgDevicesTD} content={this.renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
-											<TC content={
-												<ItemGrid container zeroMargin noPadding alignItems={'center'}>
-													<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
-														<Info noWrap paragraphCell={classes.noMargin}>
-															{`${n.name} - ${n.id}`}
-														</Info>
-													</ItemGrid>
-													<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
-														<Caption noWrap className={classes.noMargin}>
 
-														</Caption>
-													</ItemGrid>
-													{/* </ItemGrid> */}
+	const { collections, classes } = props
+	return (
+		<InfoCard
+			title={t('collections.pageTitle')}
+			subheader={<ItemG><Caption>
+				{`${t('orgs.fields.collectionCount')}: ${collections.length}`}
+			</Caption></ItemG>}
+			avatar={<DataUsage />}
+			noExpand
+			noPadding
+			content={
+				<Table>
+					<TableBody style={{ padding: "0 24px" }}>
+						{collections ? collections.map((n, i) => {
+							return (
+								<TableRow
+									hover
+									onClick={e => { e.stopPropagation(); props.history.push({ pathname: '/collection/' + n.id, prevURL: `/management/org/${props.org.id}` }) }}
+									key={i}
+									style={{ cursor: 'pointer', padding: '0 20px' }}
+								>
+									<Hidden lgUp>
+										<TC checkbox className={classes.orgDevicesTD} content={renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
+										<TC content={
+											<ItemGrid container zeroMargin noPadding alignItems={'center'}>
+												<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
+													<Info noWrap paragraphCell={classes.noMargin}>
+														{`${n.name} - ${n.id}`}
+													</Info>
 												</ItemGrid>
-											} />
+												<ItemGrid zeroMargin noPadding zeroMinWidth xs={12}>
+													<Caption noWrap className={classes.noMargin}>
 
-										</Hidden>
-										<Hidden mdDown>
-											<TC checkbox className={classes.orgDevicesTD} content={this.renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
-											<TC checkbox label={n.id} />
-											<TC label={n.name} />
-										</Hidden>
-									</TableRow>
+													</Caption>
+												</ItemGrid>
+												{/* </ItemGrid> */}
+											</ItemGrid>
+										} />
 
-								)
-							}) : null}
-						</TableBody>
-					</Table>
-				} />
-		)
-	}
+									</Hidden>
+									<Hidden mdDown>
+										<TC checkbox className={classes.orgDevicesTD} content={renderIcon(n.activeDeviceStats ? n.activeDeviceStats.state : null)} />
+										<TC checkbox label={n.id} />
+										<TC label={n.name} />
+									</Hidden>
+								</TableRow>
+
+							)
+						}) : null}
+					</TableBody>
+				</Table>
+			} />
+	)
 }
 
 export default withStyles(devicetableStyles)(OrgCollections)
