549f53615e4cdda2fb392deda73d6b813f84e33e
SromPavel <1074227@ucn.dk>
2020-02-13 12:21:31 +0100

    ChartDataPanel.js => hooks

--- src/views/Charts/ChartDataPanel.js
+++ src/views/Charts/ChartDataPanel.js
@@ -1,78 +1,80 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { InfoCard, DateFilterMenu, Dropdown, T, ItemG } from 'components';
-import { withStyles, Button, IconButton, Typography } from '@material-ui/core';
+import { makeStyles, Button, IconButton, Typography } from '@material-ui/core';
 import { Add, Visibility, VisibilityOff, Clear, Timeline } from 'variables/icons';
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { hideShowPeriod, resetToDefault } from 'redux/dateTime';
 import { dateTimeFormatter } from 'variables/functions';
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
-import withLocalization from 'components/Localization/T';
+// import withLocalization from 'components/Localization/T';
+import { useLocalization } from 'hooks'
 
-const styles = theme => ({
+const styles = makeStyles(theme => ({
 	icon: {
-
 		color: theme.palette.type === 'light' ? 'rgba(0, 0, 0, 0.54)' : '#fff',
 		marginRight: 4
 	}
-})
-class ChartDataPanel extends Component {
-	render() {
-		const { t, periods, classes, width } = this.props
-		let mobile = isWidthUp('md', width)
-		return (
-			<InfoCard
-				title={t('charts.controlPanel')}
-				avatar={<Timeline />}
-				noExpand
-				noMargin
-				noPadding
-				content={null}
-				topAction={
-					<Fragment>
-						{periods.length < 5 &&
-							<DateFilterMenu
-								button={mobile}
-								icon={<ItemG container alignItems={'center'}><Add className={classes.icon} />{!mobile ? null : <T className={classes.icon}>{t('menus.charts.addAPeriod')}</T>}</ItemG>}
-								t={t}
-							/>
-						}
+}))
 
-						<Dropdown
-							icon={<Fragment><Visibility className={classes.icon} />{!mobile ? null : <T className={classes.icon}>{t('menus.charts.showHidePeriods')}</T>}</Fragment>}
+// const mapStateToProps = (state) => ({
+// 	periods: state.dateTime.periods
+// })
+
+// const mapDispatchToProps = dispatch => ({
+// 	hideShowPeriod: pId => dispatch(hideShowPeriod(pId)),
+// 	resetToDefault: () => dispatch(resetToDefault())
+// })
+
+// @Andrei
+const ChartDataPanel = props => {
+	const classes = styles()
+	const t = useLocalization()
+	const dispatch = useDispatch()
+	const periods = useSelector(state => state.dateTime.periods)
+
+	const { width } = props
+	let mobile = isWidthUp('md', width)
+	return (
+		<InfoCard
+			title={t('charts.controlPanel')}
+			avatar={<Timeline />}
+			noExpand
+			noMargin
+			noPadding
+			content={null}
+			topAction={
+				<Fragment>
+					{periods.length < 5 &&
+						<DateFilterMenu
 							button={mobile}
-							divider
-							menuItems={
-								periods.map(p => ({
-									label: <T style={{ whiteSpace: 'normal' }}>{`${dateTimeFormatter(p.from)} - ${dateTimeFormatter(p.to)}`}</T>,
-									icon: p.hide ? VisibilityOff : Visibility,
-									func: () => this.props.hideShowPeriod(p.id)
-								}))}
+							icon={<ItemG container alignItems={'center'}><Add className={classes.icon} />{!mobile ? null : <T className={classes.icon}>{t('menus.charts.addAPeriod')}</T>}</ItemG>}
+							t={t}
 						/>
-						{mobile ?
-							<Button onClick={this.props.resetToDefault}>
-								<Clear className={classes.icon} />
-								<Typography>{t('menus.charts.resetToDefault')}</Typography>
-							</Button>
-							: <IconButton onClick={this.props.resetToDefault}>
-								<Clear className={classes.icon} />
-							</IconButton>}
-					</Fragment>
-				}
-			/>
-		)
-	}
+					}
+
+					<Dropdown
+						icon={<Fragment><Visibility className={classes.icon} />{!mobile ? null : <T className={classes.icon}>{t('menus.charts.showHidePeriods')}</T>}</Fragment>}
+						button={mobile}
+						divider
+						menuItems={
+							periods.map(p => ({
+								label: <T style={{ whiteSpace: 'normal' }}>{`${dateTimeFormatter(p.from)} - ${dateTimeFormatter(p.to)}`}</T>,
+								icon: p.hide ? VisibilityOff : Visibility,
+								func: () => dispatch(hideShowPeriod(p.id))
+							}))}
+					/>
+					{mobile ?
+						<Button onClick={() => dispatch(resetToDefault())}>
+							<Clear className={classes.icon} />
+							<Typography>{t('menus.charts.resetToDefault')}</Typography>
+						</Button>
+						: <IconButton onClick={() => dispatch(resetToDefault())}>
+							<Clear className={classes.icon} />
+						</IconButton>}
+				</Fragment>
+			}
+		/>
+	)
 }
-const mapStateToProps = (state) => ({
-	periods: state.dateTime.periods
-})
 
-const mapDispatchToProps = dispatch => ({
-	hideShowPeriod: pId => dispatch(hideShowPeriod(pId)),
-	resetToDefault: () => dispatch(resetToDefault())
-})
-
-
-export default withLocalization()(
-	withWidth()(
-		withStyles(styles, { withTheme: true })(
-			connect(mapStateToProps, mapDispatchToProps)(ChartDataPanel))))
+export default withWidth()(ChartDataPanel)
