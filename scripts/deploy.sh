337a9d589ac4fb47045da5a8006fb31ae5f8a3dc
Andrei Tudor <andrei.tudor27@gmail.com>
2020-04-24 11:16:56 +0200

    Try again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,24 +22,24 @@ if [[ "$1" == "beta" ]]; then
 	exit 0
 fi
 
-if [[ "$1" == "alpha" ]]; then
-	echo
-	echo Deploying Senti App $1 ...
-	# rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/odeumcode/beta.senti.cloud
-	echo
-	# Senti Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti App ALPHA updated!"}' $3
-	echo Deployment to beta done!
-	exit 0
-fi
+# if [[ "$1" == "alpha" ]]; then
+# 	echo
+# 	echo Deploying Senti App $1 ...
+# 	# rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/odeumcode/beta.senti.cloud
+# 	echo
+# 	# Senti Workspace
+# 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti App ALPHA updated!"}' $3
+# 	echo Deployment to beta done!
+# 	exit 0
+# fi
 
 if [[ "$1" == "alpha-dev" ]]; then
 	echo
 	echo Deploying Senti App $1 ...
-	rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/odeumcode/alpha.senti.cloud
+	rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/odeumcode/alpha.senti.cloud/
 	echo
 	# Senti Workspace
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti App ALPHA updated!"}' $3
-	echo Deployment to beta done!
+	echo Deployment to alpha-dev done!
 	exit 0
 fi
