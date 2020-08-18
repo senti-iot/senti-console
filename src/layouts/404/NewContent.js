f276be0c221b451d99fdbe376e82acc2c80aa98c
Andrei Tudor <andrei.tudor27@gmail.com>
2020-08-18 12:04:49 +0200

    Test isBeta

--- src/layouts/404/NewContent.js
+++ src/layouts/404/NewContent.js
@@ -5,16 +5,28 @@ import SnackbarContent from '@material-ui/core/SnackbarContent'
 import Warning from '@material-ui/icons/Warning'
 import amber from '@material-ui/core/colors/amber';
 import styled from 'styled-components'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { ItemG } from 'components'
+import { closeIsBeta } from 'redux/serviceWorkerRedux'
+import { Close } from 'variables/icons'
+import { lightGreen } from '@material-ui/core/colors'
 
 const IconWarn = styled(Warning)`
 	color: #ffffff;
-	font-size: 16px;
+	font-size: 24px;
 	opacity: 0.9;
 	margin-right: 8px;
 `
 const UpdateSnackbar = styled(SnackbarContent)`
 	background: ${amber[700]};
+`
+const SuccessButton = styled(Button)`
+	color: #ffffff;
+	padding: 8px;
+	background :#4caf50;
+	&:hover {
+    	background: ${lightGreen[500]};
+  	}
 `
 const RefreshButton = styled(Button)`
 	color: #ffffff;
@@ -24,35 +36,84 @@ const RefreshButton = styled(Button)`
   	}
 `
 const Message = styled.span`
+	max-width: 800px;
 	color: #ffffff;
 	display: flex;
 	align-items: center;
 `
 
 function NewContent(props) {
+	const dispatch = useDispatch()
+	// const history = useHistory()
 	const serviceWorkerUpdated = useSelector(s => s.serviceWorkerReducer.serviceWorkerUpdated)
+	const isBeta = useSelector(s => s.serviceWorkerReducer.isBeta)
 	const handleClose = () => {
 		window.location.reload()
 	};
+	const handleBetaClose = () => {
+		dispatch(closeIsBeta())
+	}
+	const handleGoToApp = () => {
+		window.location.href = 'https://app.senti.cloud'
+	}
 	return (
-
-		<Snackbar
-			anchorOrigin={{
-				vertical: 'top',
-				horizontal: 'center',
-			}}
-			open={serviceWorkerUpdated}
-		>
-			<UpdateSnackbar
-				message={<Message>
-					<IconWarn />
-					{props.installing ? 'Caching application ...' : 'Update Available'}
-				</Message>}
-				action={<RefreshButton size="small" onClick={handleClose}>
-					REFRESH</RefreshButton>}
+		<>
+			<Snackbar
+				anchorOrigin={{
+					vertical: 'top',
+					horizontal: 'center',
+				}}
+				open={serviceWorkerUpdated}
 			>
-			</UpdateSnackbar>
-		</Snackbar>
+				<UpdateSnackbar
+					message={<Message>
+						<IconWarn />
+						{'Update Available'}
+					</Message>}
+					action={<RefreshButton size="small" onClick={handleClose}>
+					REFRESH</RefreshButton>}
+				>
+				</UpdateSnackbar>
+			</Snackbar>
+			<Snackbar
+				anchorOrigin={{
+					vertical: 'top',
+					horizontal: 'center',
+				}}
+				open={isBeta}
+			>
+				<UpdateSnackbar
+					message={
+						<ItemG container>
+							<ItemG xs={1} container justify={'center'} alignItems={'center'}>
+								<IconWarn />
+							</ItemG>
+							<ItemG xs container alignItems={'center'} >
+								<Message>
+									{`You are running on the \`beta\` version of Senti!`}
+								</Message>
+								<Message>
+							Please consider switching to the stable version at https://app.senti.cloud
+								</Message>
+							</ItemG>
+							<ItemG >
+								<SuccessButton size="small" onClick={handleGoToApp}>
+									Go to App
+								</SuccessButton>
+								<RefreshButton onClick={handleBetaClose}>
+									<Close />
+								</RefreshButton>
+							</ItemG>
+						</ItemG>
+					}
+					// action={
+
+					// }
+				>
+				</UpdateSnackbar>
+			</Snackbar>
+		</>
+
 	);
 }
 
--- src/redux/serviceWorkerRedux.js
+++ src/redux/serviceWorkerRedux.js
@@ -1,16 +1,22 @@
 
 // CONSTANTS
 export const UPDATE_SERVICEWORKER = 'UPDATE_SERVICEWORKER'
-
+const CloseIsBeta = "CloseIsBetaBanner"
+export function closeIsBeta() {
+	return {
+		type: CloseIsBeta
+	}
+}
 export function updateServiceworker() {
 	return {
 		type: UPDATE_SERVICEWORKER
 	}
 }
-
-export const serviceWorkerReducer = (state = {
-	serviceWorkerUpdated: false
-}, action) => {
+const initialState = {
+	serviceWorkerUpdated: false,
+	isBeta: process.env.isBeta ? process.env.isBeta : false
+}
+export const serviceWorkerReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case UPDATE_SERVICEWORKER: {
 			return {
@@ -18,6 +24,11 @@ export const serviceWorkerReducer = (state = {
 				serviceWorkerUpdated: true
 			}
 		}
+		case CloseIsBeta:
+			return {
+				...state,
+				isBeta: false
+			}
 		default:
 			return state
 	}
